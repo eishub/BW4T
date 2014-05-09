49785c964b0f5ded01c31bc3f7b9559de07bbb29
LiebenS <sander.liebens@home.nl>
2014-05-09 10:55:29 +0200

    Nu in de goede map...
    Former-commit-id: 6c007df00d3e3a6f528602d096ccfc5071435941

--- bw4t/bw4t-client/src/main/java/client/BW4TClient.java
+++ bw4t/bw4t-client/src/main/java/client/BW4TClient.java
@@ -1,6 +1,37 @@
+package client;
 
-public class BW4TClient extends UnicastRemoteObject implements BW4TClientInterface{
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
+/**
+ * The client class via which all the communication travels to the server.
+ * @author Sander Liebens
+ */
+
+public class BW4TClient extends UnicastRemoteObject implements BW4TClientInterface {
 	
+	private static final long serialVersionUID = 4060908402879332038L;
+	
+	/**
+	 * The parent that we serve.
+	 * @ToDo: check whether we need this, or how we are going to work around this.
+	 */
+	//private BW4TRemoteEnvironment parent
+	
+	private String bindAddress;
+	
+	//@ToDo: make this work with BW4TServerInterface.
+	//private BW4TServerInterface server;
+	
+	//@ToDO: Still using NewMap???
+	// private NewMap map;
+	
+	protected BW4TClient(BW4TRemoteEnvironment parent) throws RemoteException, 
+				MalformedURLException, NotBoundException {
+		this.parent = parent
+		
+	}
 	
 }
