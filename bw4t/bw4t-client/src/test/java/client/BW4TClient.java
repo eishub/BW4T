e8ef60095d79e574ed6e861a93a5fd9c3584eac7
LiebenS <sander.liebens@home.nl>
2014-05-09 10:38:57 +0200

    Added a Client class.
    Former-commit-id: 8df05cd9a038df48c8094b0e3164e1c9cecf2422

---
+++ bw4t/bw4t-client/src/test/java/client/BW4TClient.java
@@ -0,0 +1,28 @@
+package client;
+
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * The client class via which all the communication travels to the server.
+ * @author Sander Liebens
+ */
+
+public class BW4TClient extends UnicastRemoteObject implements BW4TClientInterface {
+	
+	private static final long serialVersionUID = 4060908402879332038L;
+	
+	/**
+	 * The parent that we serve.
+	 * @ToDo: check whether we need this, or how we are going to work around this.
+	 */
+	//private BW4TRemoteEnvironment parent
+	
+	private String bindAddress;
+	
+	//@ToDo: make this work with BW4TServerInterface.
+	//private BW4TServerInterface server;
+	
+	//@ToDO: Still using NewMap???
+	// private NewMap map;
+	
+}
