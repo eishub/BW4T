0bfec24561803ab65393cca4f7db9bebb9b4aa58
Sille Kamoen <sille.kamoen@gmail.com>
2014-05-12 10:34:51 +0200

    Implemented registerClient() and dependencies
    Former-commit-id: a3713c98f7b4c6b3c2ea6528c663ab06946121d6

--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/BW4TServerInterface.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/BW4TServerInterface.java
@@ -2,6 +2,11 @@ package nl.tudelft.bw4t.server;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Collection;
+
+import eis.exceptions.EntityException;
+import eis.iilang.EnvironmentState;
+import nl.tudelft.bw4t.client.BW4TClient;
 
 /**
  * Interface of the actions performed by BW4TServer.
@@ -16,5 +21,42 @@ public interface BW4TServerInterface extends Remote {
      *             if the remote was not responding correctly.
      */
     String say() throws RemoteException;
+    
+	/**
+	 * Called when a client wants to register itself to this server
+	 * 
+	 * @param client
+	 *            the client that wants to register
+	 * @param agentCount
+	 *            the amount of agent entities that the client is waiting for
+	 * @param humanCount
+	 *            the amount of human GUI entities that the client is waiting
+	 *            for
+	 * @throws RemoteException
+	 *             if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	void registerClient(BW4TClient client, int agentCount, int humanCount)
+			throws RemoteException;
+	
+	/**
+	 * Get the state of the environment
+	 * 
+	 * @return the state of the environment
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	EnvironmentState getState() throws RemoteException;
+	
+	/**
+	 * Get all free entities on the server
+	 * 
+	 * @return all free entities
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	Collection<String> getFreeEntities() throws RemoteException;
 
 }
