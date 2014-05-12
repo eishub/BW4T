b53be04fd308153cd4933423176f3af1e5789c74
Sille Kamoen <sille.kamoen@gmail.com>
2014-05-12 11:26:53 +0200

    Implemented all server methods

--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/BW4TServerInterface.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/BW4TServerInterface.java
@@ -3,10 +3,20 @@ package nl.tudelft.bw4t.server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 
-import eis.exceptions.EntityException;
-import eis.iilang.EnvironmentState;
+import repast.simphony.batch.BatchRunner;
 import nl.tudelft.bw4t.client.BW4TClient;
+import eis.EnvironmentInterfaceStandard;
+import eis.exceptions.AgentException;
+import eis.exceptions.EntityException;
+import eis.exceptions.ManagementException;
+import eis.exceptions.RelationException;
+import eis.iilang.Action;
+import eis.iilang.EnvironmentState;
+import eis.iilang.Parameter;
+import eis.iilang.Percept;
 
 /**
  * Interface of the actions performed by BW4TServer.
@@ -58,5 +68,262 @@ public interface BW4TServerInterface extends Remote {
 	 *             object call
 	 */
 	Collection<String> getFreeEntities() throws RemoteException;
+	
+	/**
+	 * Remove a client from the server. At this moment the client makes sure
+	 * that it frees its entities first.
+	 * 
+	 * @param client
+	 * @throws RemoteException
+	 */
+	void unregisterClient(BW4TClient client) throws RemoteException;
+	
+	/**
+	 * Called when a client wants to perform an entity action
+	 * 
+	 * @param entity
+	 *            , the entity that should perform the action
+	 * @param action
+	 *            , the action that should be performed
+	 * @throws RemoteException
+	 *             if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	public Percept performEntityAction(String entity, Action action)
+			throws RemoteException;
+	
+	/**
+	 * Called when a client wants to associate an agent to an entity
+	 * 
+	 * @param agentId
+	 *            , the agent that should be associated to the entity
+	 * @param entityId
+	 *            , the entity that the agent should be associated to
+	 * @throws RelationException
+	 *             if the attempt to manipulate the agents-entities-relation has
+	 *             failed.
+	 */
+	void associateEntity(String agentId, String entityId)
+			throws RelationException, RemoteException;
+	
+	/**
+	 * Called when a client wants to register an agent
+	 * 
+	 * @param agentId
+	 *            , the agent that should be registered
+	 * @throws RemoteException
+	 *             if an exception occurs during the execution of a remote
+	 *             object call
+	 * @throws AgentException
+	 *             if the attempt to register or unregister an agent has failed.
+	 */
+	void registerAgent(String agentId) throws RemoteException, AgentException;
+	
+	/**
+	 * Called when a client wants to unregister an agent
+	 * 
+	 * @param agent
+	 *            , the agent that should be unregistered
+	 * @throws AgentException
+	 *             if the attempt to register or unregister an agent has failed.
+	 */
+	void unregisterAgent(String agent) throws AgentException, RemoteException;
+	
+	/**
+	 * Called when a client wants to get all percepts from an entity
+	 * 
+	 * @param entity
+	 *            , the entity for which all percepts should be gotten
+	 * @return a list of all gotten percepts
+	 * @throws RemoteException
+	 *             if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	LinkedList<Percept> getAllPerceptsFromEntity(String entity)
+			throws RemoteException;
+	
+	/**
+	 * Get all agents registered on the server
+	 * 
+	 * @return a list of agent names
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	LinkedList<String> getAgents() throws RemoteException;
+	
+	/**
+	 * Get all associated entities for a certain agent from the server
+	 * 
+	 * @param agent
+	 *            , the agent
+	 * @return a list of entity names
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 * @throws AgentException
+	 *             , if an attempt to register or unregister an agent has
+	 *             failed.
+	 */
+	HashSet<String> getAssociatedEntities(String agent) throws RemoteException,
+			AgentException;
 
+	/**
+	 * Get all the entities on the server
+	 * 
+	 * @return the list of entities
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	Collection<String> getEntities() throws RemoteException;
+	
+	/**
+	 * Free an entity on the server
+	 * 
+	 * @param entity
+	 *            , the entity to free
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 * @throws RelationException
+	 *             , if an attempt to manipulate the agents-entities-relation
+	 *             has failed.
+	 * @throws EntityException
+	 *             , if something unexpected happens when attempting to add or
+	 *             remove an entity.
+	 */
+	void freeEntity(String entity) throws RemoteException, RelationException,
+			EntityException;
+	
+	/**
+	 * Free an agent on the server
+	 * 
+	 * @param agent
+	 *            , the agent to free
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 * @throws RelationException
+	 *             , if an attempt to manipulate the agents-entities-relation
+	 *             has failed.
+	 */
+	void freeAgent(String agent) throws RemoteException, RelationException;
+	
+	/**
+	 * Free an agent-entity pair on the server
+	 * 
+	 * @param agent
+	 *            , the agent for which an entity should be freed
+	 * @param entity
+	 *            , the entity to be freed
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 * @throws RelationException
+	 *             , if an attempt to manipulate the agents-entities-relation
+	 *             has failed.
+	 */
+	void freePair(String agent, String entity) throws RemoteException,
+			RelationException;
+
+	/**
+	 * Get all associated agents for a certain entity from the server
+	 * 
+	 * @param entity
+	 *            , the entity for which to get all associated agents
+	 * @return the list of agents
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 * @throws EntityException
+	 *             , if something unexpected happens when attempting to add or
+	 *             remove an entity.
+	 */
+	Collection<String> getAssociatedAgents(String entity)
+			throws RemoteException, EntityException;
+	
+	/**
+	 * Get the type of an entity
+	 * 
+	 * @param entity
+	 *            , the entity for which to get its type
+	 * @return the type of the entity
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 * @throws EntityException
+	 *             , if something unexpected happens when attempting to add or
+	 *             remove an entity.
+	 */
+	String getType(String entity) throws RemoteException, EntityException;
+	
+	/**
+	 * Query a property of the server environment
+	 * 
+	 * @param property
+	 *            , the property to query
+	 * @return the property's value
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	String queryProperty(String property) throws RemoteException;
+	
+	/**
+	 * Query a property of an entity on the server
+	 * 
+	 * @param entity
+	 *            , the entity for which to query the property
+	 * @param property
+	 *            , the property to query
+	 * @return the value of the property
+	 * @throws RemoteException
+	 *             , if an exception occurs during the execution of a remote
+	 *             object call
+	 */
+	String queryEntityProperty(String entity, String property)
+			throws RemoteException;
+	/**
+	 * TODO: Write comments
+	 * @param arg0
+	 * @return
+	 * @throws RemoteException
+	 */
+	boolean isSupportedByEnvironment(Action arg0) throws RemoteException;
+	
+	/**
+	 * request the server to start environment.
+	 */
+	void requestStart() throws RemoteException, ManagementException;
+
+	/**
+	 * request the server to pause environment.
+	 */
+	void requestPause() throws RemoteException, ManagementException;
+
+	/**
+	 * initialize the server. Does not reset the connection, but loads map and
+	 * resets repast
+	 * 
+	 * @param parameters
+	 *            init parameters for eis. see
+	 *            {@link EnvironmentInterfaceStandard#init(java.util.Map)}
+	 */
+	void requestInit(java.util.Map<String, Parameter> parameters)
+			throws RemoteException, ManagementException;
+
+	/**
+	 * * reset the server, following the requirements for {@link BatchRunner}.
+	 * This means that no new entities are created, but the old ones are left
+	 * connected.
+	 * 
+	 * @param parameters
+	 *            init parameters for eis. see
+	 *            {@link EnvironmentInterfaceStandard#init(java.util.Map)}
+	 */
+	void requestReset(java.util.Map<String, Parameter> parameters)
+			throws RemoteException, ManagementException;
+
+	
 }
--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/BW4TEnvironment.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/BW4TEnvironment.java
@@ -1,8 +1,17 @@
 package nl.tudelft.bw4t.server.environment;
 
+import java.util.LinkedList;
+
+import nl.tudelft.bw4t.BW4TBuilder;
 import nl.tudelft.bw4t.map.NewMap;
+import nl.tudelft.bw4t.server.BW4TLogger;
+import nl.tudelft.bw4t.server.RobotEntityInterface;
 import eis.eis2java.environment.AbstractEnvironment;
+import eis.exceptions.ActException;
+import eis.exceptions.NoEnvironmentException;
+import eis.exceptions.PerceiveException;
 import eis.iilang.Action;
+import eis.iilang.Percept;
 
 /**
  * The BW4TEnvironement, which includes and describes the whole
@@ -25,6 +34,10 @@ public class BW4TEnvironment extends AbstractEnvironment {
      */
     private static NewMap theMap;
     
+    /**
+     * Check if the map is fully loaded
+     */
+	private boolean mapFullyLoaded;
 	/**
 	 * Get the instance of this environment
 	 * 
@@ -46,18 +59,73 @@ public class BW4TEnvironment extends AbstractEnvironment {
 	public static NewMap getMap() {
 		return theMap;
 	}
+	
+	/**
+	 * Helper method to allow the server to call actions received from attached
+	 * clients
+	 * 
+	 * @param entity
+	 *            , the entity that should perform the action
+	 * @param action
+	 *            , the action that should be performed
+	 * @return the percept received after performing the action
+	 */
+	public Percept performClientAction(String entity, Action action)
+			throws ActException {
+		BW4TLogger.getInstance().logAction(entity, action.toProlog());
+		return performEntityAction(entity, action);
+	}
 
-    /**
-     * No idea... should still be implemented.
-     * @param arg0
-     *            No Idea.
-     * @return No Idea.
-     */
-    @Override
-    protected final boolean isSupportedByEnvironment(final Action arg0) {
-        return false;
-    }
-
+	/**
+	 * Helper method to allow the server to get all percepts for a connected
+	 * client.
+	 * <p>
+	 * This function is synchronized to ensure that multiple calls are properly
+	 * sequenced. This is important because getAllPercepts must 'lock' the
+	 * environment and parallel calls would cause overlapping 'locks' taken at
+	 * different moments in time.
+	 * <p>
+	 * Actually, locking the environment is done by copying the current location
+	 * of the entity.
+	 * <p>
+	 * It seems that this new function is created because
+	 * {@link AbstractEnvironment#getAllPerceptsFromEntity(String)} is final.
+	 * 
+	 * TODO: Cast to Robotinterface
+	 * @param entity
+	 *            , the entity for which all percepts should be gotten
+	 * @return all percepts for the entity
+	 */
+	public synchronized LinkedList<Percept> getAllPerceptsFrom(String entity) {
+		try {
+			if (this.isMapFullyLoaded()) {
+				((RobotEntityInterface) getEntity(entity))
+						.initializePerceptionCycle();
+				return getAllPerceptsFromEntity(entity);
+			}
+		} catch (PerceiveException e) {
+			e.printStackTrace();
+		} catch (NoEnvironmentException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Check if the map was fully loaded. When this is true, all entities also
+	 * have been processed and the environment is ready to run. Note that
+	 * because of #2016 there may be an async between Repast and this
+	 * BW4TEnvironment, causing this flag to remain true while repast has in
+	 * fact stopped. We detect this only when the user turns 'on' the Repast
+	 * environment again, in {@link BW4TBuilder#build()}.
+	 * 
+	 * @return
+	 */
+	public boolean isMapFullyLoaded() {
+		return mapFullyLoaded;
+	}
+	
+	
     /**
      * No idea... should still be implemented.
      * @param arg0
@@ -71,5 +139,19 @@ public class BW4TEnvironment extends AbstractEnvironment {
             final String arg1) {
         return false;
     }
+    
+    /**
+	 * Check whether an action is supported by this environment.
+	 * 
+	 * @param arg0
+	 *            , the action that should be checked
+	 * @return true if there is an entity, a dropzone and sequence not yet
+	 *         complete
+	 */
+	@Override
+	public boolean isSupportedByEnvironment(Action arg0) {
+
+		return !getEntities().isEmpty();
+	}
 
 }
