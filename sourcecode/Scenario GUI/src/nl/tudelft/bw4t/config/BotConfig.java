178df5da64627c0f36a329a223c6e36a0c4a0f57
Nick Feddes <gnick.com@gmail.com>
2014-05-13 10:10:59 +0200

    Changes package names at Calvin's request.

--- sourcecode/Scenario GUI/src/nl/tudelft/bw4t/config/BotConfig.java
+++ sourcecode/Scenario GUI/src/nl/tudelft/bw4t/config/BotConfig.java
@@ -1,4 +1,4 @@
-package nl.tudelft.bw4t.scenariogui;
+package nl.tudelft.bw4t.config;
 
 public class BotConfig {
 	
--- sourcecode/Scenario GUI/src/nl/tudelft/config/BotConfig.java
+++
@@ -1,8 +0,0 @@
-package nl.tudelft.config;
-
-public class BotConfig {
-	
-	//TODO: whatever options you guys put in your Botstore GUI that are bot specific
-	//and you want to get saved, put it here as variables (so we can convert this to xml)
-
-}
