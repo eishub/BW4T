9146a9f77706b180a278f4b7d48014cfa4acf246
Katia Asmoredjo <katia.asmoredjo@gmail.com>
2014-05-13 19:01:21 +0200

    Added dialogs

--- sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/gui/panel/BotPanel.java
+++ sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/gui/panel/BotPanel.java
@@ -21,13 +21,17 @@ public class BotPanel extends JPanel implements ActionListener {
     
     private JPanel botCountInfo = new JPanel();
     private JPanel botOptions = new JPanel();
-    private JScrollPane botTable;
+    private JTable botTable;
+    private JScrollPane botScrollPane;
     
     private JButton newBot = new JButton("New bot");
     private JButton modifyBot = new JButton("Modify bot");
     private JButton renameBot = new JButton("Rename bot");
     private JButton duplicateBot = new JButton("Duplicate bot");
     private JButton deleteBot = new JButton("Delete bot");
+
+    private JTextField newBotName = new JTextField();
+    private JTextField duplicateAmount = new JTextField();
     
     public BotPanel(){
         setLayout(new BorderLayout(5, 5));
@@ -39,7 +43,7 @@ public class BotPanel extends JPanel implements ActionListener {
         
         add(botCountInfo, BorderLayout.NORTH);
         add(botOptions, BorderLayout.EAST);
-        add(botTable, BorderLayout.CENTER);
+        add(botScrollPane, BorderLayout.CENTER);
     }
     
     private void createBotCountPanel(){
@@ -69,30 +73,24 @@ public class BotPanel extends JPanel implements ActionListener {
     private void createBotOptionPanel(){
     	botOptions.setLayout(new GridLayout(15, 2));
         
-        //JPanel rightBotPanel = new JPanel();
-        //rightBotPanel.setLayout(new GridLayout(5, 2));
-        
-        newBot.addActionListener(this);
-        modifyBot.addActionListener(this);
-        renameBot.addActionListener(this);
-        duplicateBot.addActionListener(this);
-        deleteBot.addActionListener(this);
-        
         botOptions.add(newBot);
         botOptions.add(new JLabel(""));
         botOptions.add(modifyBot);
         botOptions.add(new JLabel(""));
-        botOptions.add(new JTextField(20));
+        botOptions.add(newBotName);
         botOptions.add(renameBot);
         botOptions.add(new JLabel(""));
-        botOptions.add(new JTextField(5));
+        duplicateAmount.setText("1");
+        botOptions.add(duplicateAmount);
         botOptions.add(duplicateBot);
         botOptions.add(new JLabel(""));
         botOptions.add(deleteBot);
+        
+        addActionListeners();
     }
     
     private void createBotTablePanel(){
-        JTable table = new JTable();
+        JTable botTable = new JTable();
         DefaultTableModel botInfo = new DefaultTableModel(){
             @Override
             public boolean isCellEditable(int row, int column){
@@ -101,11 +99,19 @@ public class BotPanel extends JPanel implements ActionListener {
             }
         };
         
-        table.setModel(botInfo);
+        botTable.setModel(botInfo);
         botInfo.addColumn("Bot");
         botInfo.addColumn("Type");
         
-        botTable = new JScrollPane(table);
+        botScrollPane = new JScrollPane(botTable);
+    }
+    
+    private void addActionListeners(){
+        newBot.addActionListener(this);
+        modifyBot.addActionListener(this);
+        renameBot.addActionListener(this);
+        duplicateBot.addActionListener(this);
+        deleteBot.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent ae) {
@@ -114,18 +120,26 @@ public class BotPanel extends JPanel implements ActionListener {
         }
         
         if(ae.getSource() == modifyBot){
-            System.out.println("Got to Bot Store");
+            System.out.println("Go to Bot Store");
         }
         
-        if(ae.getSource() == renameBot){}
+        if(ae.getSource() == renameBot){
+        	if(newBotName.getText().length() == 0){
+        		JOptionPane.showMessageDialog(null,  "Please enter a name.");
+        	} else {}
+        }
         
-        if(ae.getSource() == duplicateBot){}
+        if(ae.getSource() == duplicateBot){
+        	if(duplicateAmount.getText().length() == 0){
+        		JOptionPane.showMessageDialog(null, "Amount not specified.");
+        	} else {}
+        }
         
         if(ae.getSource() == deleteBot){
             JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this bot?", "", JOptionPane.YES_NO_OPTION);
         }
         
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet.");
     }
     
 }
