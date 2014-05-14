33171f76f633412d32f2ac1185f985dd62cf232c
Katia Asmoredjo <katia.asmoredjo@gmail.com>
2014-05-14 14:10:17 +0200

    Added javadoc

--- sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/gui/panel/ConfigurationPanel.java
+++ sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/gui/panel/ConfigurationPanel.java
@@ -45,61 +45,105 @@ public class ConfigurationPanel extends JPanel {
         
     }
     
+    /**
+     * Show the "Configuration" label in the panel.
+     */
     private void showConfigLabel(){
     	add(new JLabel("Configuration")); add(new JLabel(" ")); add(new JLabel(" ")); add(new JLabel(" "));
     }
     
+    /**
+     * Show the client config options in the panel.
+     */
     private void showClientOptions(){
     	add(new JLabel("Client")); add(new JLabel(" ")); add(new JLabel(" ")); add(new JLabel(" "));
         add(new JLabel("IP")); add(clientIP); add(new JLabel("Port")); add(clientPort);
     }
     
+    /**
+     * Show the serer config options in the panel.
+     */
     private void showServerOptions(){
         add(new JLabel("Server")); add(new JLabel(" ")); add(new JLabel(" ")); add(new JLabel(" "));
         add(new JLabel("IP")); add(serverIP); add(new JLabel("Port")); add(serverPort);
     }
     
+    /**
+     * Show the option to use GOAL in the panel.
+     */
     private void showGoalOptions(){
         add(new JLabel("Use GOAL")); add(new JLabel(" ")); add(new JLabel(" ")); add(new JLabel(" "));
         add(goalYes); add(goalNo); add(new JLabel(" ")); add(new JLabel(" "));
     }
     
+    /**
+     * Show the option to use a GUI in the panel.
+     */
     private void showGuiOptions(){
         add(new JLabel("Launch GUI")); add(new JLabel(" ")); add(new JLabel(" ")); add(new JLabel(" "));
         add(guiYes); add(guiNo); add(new JLabel(" ")); add(new JLabel(" "));
     }
     
+    /**
+     * Show the option to add an agent class in the panel.
+     */
     private void showAgentOptions(){
         add(new JLabel("Agent class")); add(new JLabel(" ")); add(new JLabel(" ")); add(new JLabel(" "));
         add(agentClassFileTextField); add(chooseAgentFile); add(new JLabel(" ")); add(new JLabel(" "));
     }
     
+    /**
+     * Show the options to add a map file in the panel.
+     */
     private void showMapOptions(){
         add(new JLabel("Map file")); add(new JLabel(" ")); add(new JLabel(" ")); add(new JLabel(" "));
         add(mapFileTextField); add(chooseMapFile); add(new JLabel(" ")); add(new JLabel(" "));
     }
     
-
+    /**
+     * Returns the client IP.
+     * @return The client IP.
+     */
     public String getClientIP(){
     	return clientIP.getText();
     }
     
+    /**
+     * Returns the client port.
+     * @return The client port.
+     */
     public String getClientPort(){
     	return clientPort.getText();
     }
     
+    /**
+     * Returns the server IP.
+     * @return The server IP.
+     */
     public String getServerIP(){
     	return serverIP.getText();
     }
     
+    /**
+     * Returns the server port.
+     * @return The server port.
+     */
     public String getServerPort(){
     	return serverPort.getText();
     }
     
+    /**
+     * Returns if GOAL needs to be used.
+     * @return The use of GOAL.
+     */
     public boolean useGoal(){
     	return goalYes.isEnabled();
     }
     
+    /**
+     * Returns if a GUI needs to be displayed.
+     * @return The use of a GUI.
+     */
     public boolean useGui(){
     	return guiYes.isEnabled();
     }
