3d82c5b07c54339052417ce8606b9d8e5be8cdc5
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-05-14 13:34:13 +0200

    Added the BotPanel tests and changed the BotPanel a bit.

--- sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/gui/panel/BotPanel.java
+++ sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/gui/panel/BotPanel.java
@@ -1,30 +1,22 @@
 package nl.tudelft.bw4t.gui.panel;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import java.awt.*;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 /**
  * The BotPanel class represents right pane of the MainPanel.
  * It shows a list of bots and the actions that are possible to edit bots.
+ *
  * @author Katia
  */
 public class BotPanel extends JPanel {
-    
+
     private JPanel botCountInfo = new JPanel();
     private JPanel botOptions = new JPanel();
     private DefaultTableModel botList;
     private JScrollPane botScrollPane;
-    
+
     private JButton newBot = new JButton("New bot");
     private JButton modifyBot = new JButton("Modify bot");
     private JButton renameBot = new JButton("Rename bot");
@@ -32,30 +24,33 @@ public class BotPanel extends JPanel {
     private JButton deleteBot = new JButton("Delete bot");
 
     private JTextField newBotName = new JTextField();
-    private JTextField duplicateAmount = new JTextField();
-    
+
+    /* Initial value 1, maximum 100, minimum 1, steps of 1 */
+    private SpinnerModel model = new SpinnerNumberModel(1, 1, 100, 1);
+    private JSpinner duplicateAmount = new JSpinner(model);
+
     /**
      * Create a BotPanel object.
      */
-    public BotPanel(){
+    public BotPanel() {
         setLayout(new BorderLayout(5, 5));
-        
+
         createBotCountPanel();
         createBotOptionPanel();
         createBotTablePanel();
-        
-        
+
+
         add(botCountInfo, BorderLayout.NORTH);
         add(botOptions, BorderLayout.EAST);
         add(botScrollPane, BorderLayout.CENTER);
     }
-    
+
     /**
      * Create the panel that shows the current bot count.
      */
-    private void createBotCountPanel(){
+    private void createBotCountPanel() {
         botCountInfo.setLayout(new GridLayout(1, 6));
-        
+
         JLabel botLabel = new JLabel("Bots");
         JTextField botCount = new JTextField();
         botCount.setText("0");
@@ -68,7 +63,7 @@ public class BotPanel extends JPanel {
         JTextField humanCount = new JTextField();
         humanCount.setText("0");
         humanCount.setEditable(false);
-        
+
         botCountInfo.add(botLabel);
         botCountInfo.add(botCount);
         botCountInfo.add(epartnerLabel);
@@ -76,13 +71,13 @@ public class BotPanel extends JPanel {
         botCountInfo.add(humanLabel);
         botCountInfo.add(humanCount);
     }
-    
+
     /**
      * Create the panel that shows the actions that can be done.
      */
-    private void createBotOptionPanel(){
-    	botOptions.setLayout(new GridLayout(15, 2));
-        
+    private void createBotOptionPanel() {
+        botOptions.setLayout(new GridLayout(15, 2));
+
         botOptions.add(newBot);
         botOptions.add(new JLabel(""));
         botOptions.add(modifyBot);
@@ -90,130 +85,195 @@ public class BotPanel extends JPanel {
         botOptions.add(newBotName);
         botOptions.add(renameBot);
         botOptions.add(new JLabel(""));
-        duplicateAmount.setText("1");
         botOptions.add(duplicateAmount);
         botOptions.add(duplicateBot);
         botOptions.add(new JLabel(""));
         botOptions.add(deleteBot);
     }
-    
+
     /**
      * Create the panel that shows the list of bots.
      */
-    private void createBotTablePanel(){
+    private void createBotTablePanel() {
         JTable botTable = new JTable();
-        botList = new DefaultTableModel(){
+        botList = new DefaultTableModel() {
             @Override
-            public boolean isCellEditable(int row, int column){
+            public boolean isCellEditable(int row, int column) {
                 //make all cells not editable
                 return false;
             }
         };
-        
+
         botTable.setModel(botList);
         botList.addColumn("Bot");
         botList.addColumn("Type");
-        
+
         botScrollPane = new JScrollPane(botTable);
     }
         
     /*public void addBot(String botName, String botType){
-    	String[] botEntry = {botName, botType};
+        String[] botEntry = {botName, botType};
     	botList.addRow(botEntry);
     }*/
-    
+
     /**
      * Executes action that needs to happen when the "New bot" button is pressed.
      * TODO Open BotStore window
      */
-    public void addNewAction(){
-    	System.out.println("Go to Bot Store");
+    public void addNewAction() {
+        System.out.println("Go to Bot Store");
     }
-    
+
     /**
      * Executes action that needs to happen when the "Modify bot" button is pressed.
      * TODO Open BotStore window
      */
-    public void modifyAction(){
-    	System.out.println("Go to Bot Store");
+    public void modifyAction() {
+        System.out.println("Go to Bot Store");
     }
-    
+
     /**
      * Executes action that needs to happen when the "Rename bot" button is pressed.
      * TODO Save name change
      */
-    public void renameAction(){
-    	if(newBotName.getText().length() == 0){
-    		JOptionPane.showMessageDialog(null,  "Please enter a name.");
-    	} else {}
+    public void renameAction() {
+        if (newBotName.getText().length() == 0) {
+            showMessageDialog(null, "Please enter a name.");
+        } else {
+            System.out.println("Name set to " + newBotName.getText());
+        }
     }
-    
+
     /**
      * Executes action that needs to happen when the "Duplicate bot" button is pressed.
      * TODO Create new bot(s) and add to table
      */
-    public void duplicateAction(){
-    	if(duplicateAmount.getText().length() == 0){
-    		JOptionPane.showMessageDialog(null, "Amount not specified.");
-    	} else {}
+    public void duplicateAction() {
+        int count = (Integer) duplicateAmount.getValue();
+        int response = showConfirmDialog(null, "Are you sure you want to duplicate this bot " + count + " times?",
+                "Duplicate Bot Confirmation", JOptionPane.YES_NO_OPTION);
+
+        if (response == JOptionPane.YES_OPTION) {
+            System.out.println("Bot duplicated " + count + " times.");
+        }
     }
-    
+
     /**
      * Executes action that needs to happen when the "Delete bot" button is pressed.
      * TODO Delete bot from table
      */
-    public void deleteAction(){
-    	JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this bot?", "", JOptionPane.YES_NO_OPTION);
+    public void deleteAction() {
+        int response = showConfirmDialog(null, "Are you sure you want to delete this bot?", "", JOptionPane.YES_NO_OPTION);
+
+        if (response == JOptionPane.YES_OPTION) {
+            System.out.println("Bot deleted");
+        }
     }
-    
+
+    /**
+     * Create a pop up message with the the given text. The user has to press OK to continue.
+     *
+     * @param parent The parent component who's frame is to be used. If null then a frame is created.
+     * @param text   The text to be displayed in the message dialog.
+     */
+    protected void showMessageDialog(Component parent, String text) {
+        JOptionPane.showMessageDialog(parent, text);
+    }
+
+
+    /**
+     * @param parent     Determines the Frame in which the dialog is displayed; if null, or if the parentComponent has no Frame, a default Frame is used
+     * @param text       The text to display
+     * @param title      The title for the dialog
+     * @param optionType An int designating the options available on the dialog: JOptionPane.YES_NO_OPTIONS,
+     *                   JOptionPane.YES_NO_CANCEL_OPTION or JOptionPane.OK_CANCEL_OPTION,
+     * @return An int indicating the option selected by the user
+     */
+    protected int showConfirmDialog(Component parent, String text, String title, int optionType) {
+        System.out.println("much create");
+        return JOptionPane.showConfirmDialog(parent, text, title, optionType);
+    }
+
     /**
      * Returns the table with the list of bots.
+     *
      * @return The table that contains the bots.
      */
-    public DefaultTableModel getTable(){
-    	return botList;
+    public DefaultTableModel getTable() {
+        return botList;
     }
-    
+
     /**
      * Returns the button to create a new bot.
+     *
      * @return The button to create a new bot.
      */
-    public JButton getNewBot(){
-    	return newBot;
+    public JButton getNewBot() {
+        return newBot;
     }
-    
-    
+
+
     /**
      * Returns the button to modify a bot.
+     *
      * @return The button to modify a bot.
      */
-    public JButton getModifyBot(){
-    	return modifyBot;
+    public JButton getModifyBot() {
+        return modifyBot;
     }
-    
+
     /**
      * Returns the button to rename a bot.
+     *
      * @return The button to rename a bot.
      */
-    public JButton getRenameBot(){
-    	return renameBot;
+    public JButton getRenameBot() {
+        return renameBot;
     }
-    
+
     /**
      * Returns the button to duplicate a bot.
+     *
      * @return The button to duplicate a bot.
      */
-    public JButton getDuplicateBot(){
-    	return duplicateBot;
+    public JButton getDuplicateBot() {
+        return duplicateBot;
     }
-    
-     /**
-      * Returns the button to delete a bot.
-      * @return The button to delete a bot.
-      */
-    public JButton getDeleteBot(){
-    	return deleteBot;
+
+    /**
+     * Returns the button to delete a bot.
+     *
+     * @return The button to delete a bot.
+     */
+    public JButton getDeleteBot() {
+        return deleteBot;
     }
-    
+
+    /**
+     * Returns the new bot name
+     *
+     * @return The new bot name
+     */
+    public String getNewBotNameLabelText() {
+        return newBotName.getText();
+    }
+
+    /**
+     * Sets the new bot name
+     *
+     * @param newBotName The new bot name
+     */
+    public void setNewBotNameLabelText(String newBotName) {
+        this.newBotName.setText(newBotName);
+    }
+
+    /**
+     * Return the JSpinner which users can use to select the amount of duplicates.
+     *
+     * @return The JSpinner counting the duplicate amount.
+     */
+    public JSpinner getDuplicateAmount() {
+        return duplicateAmount;
+    }
 }
 
---
+++ sourcecode/Scenario GUI/src/test/java/nl/tudelft/bw4t/gui/panel/BotPanelTest.java
@@ -0,0 +1,120 @@
+package nl.tudelft.bw4t.gui.panel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+/**
+ * Created on 14-5-2014.
+ */
+public class BotPanelTest {
+
+    private MainPanel panel;
+    private BotPanel botPanel;
+    private BotPanel spyBotPanel;
+
+
+    @Before
+    public void setUp() {
+        botPanel = new BotPanel();
+        spyBotPanel = spy(botPanel);
+
+        panel = new MainPanel(new ConfigurationPanel(), spyBotPanel);
+    }
+
+    @Test
+    public void testAddNewBot() {
+        spyBotPanel.getNewBot().doClick();
+        verify(spyBotPanel, times(1)).addNewAction();
+        //TODO: Verify if the bot has actually been added.
+    }
+
+    @Test
+    public void testModifyBot() {
+        spyBotPanel.getModifyBot().doClick();
+        verify(spyBotPanel, times(1)).modifyAction();
+        //TODO: Verify if the bot has actually been modified.
+    }
+
+    @Test
+    public void testRenameBotInvalid() {
+        Mockito.doNothing().when(spyBotPanel).showMessageDialog((java.awt.Component) any(), anyString());
+
+        spyBotPanel.getRenameBot().doClick();
+        verify(spyBotPanel, times(1)).renameAction();
+        verify(spyBotPanel, times(1)).showMessageDialog((java.awt.Component) any(), anyString());
+    }
+
+    @Test
+    public void testRenameBotValid() {
+        Mockito.doNothing().when(spyBotPanel).showMessageDialog((java.awt.Component) any(), anyString());
+
+        String botname = "Quandesha";
+        spyBotPanel.setNewBotNameLabelText(botname);
+
+        spyBotPanel.getRenameBot().doClick();
+        verify(spyBotPanel, times(1)).renameAction();
+        /* Make sure no message dialog is created since nothing went wrong */
+        verify(spyBotPanel, times(0)).showMessageDialog((java.awt.Component) any(), anyString());
+
+        assertEquals(botname, spyBotPanel.getNewBotNameLabelText());
+        //TODO: Check if the name of the bot itself was actually changed.
+    }
+
+
+    @Test
+    public void testDuplicateBotYes() {
+        doReturn(JOptionPane.YES_OPTION).when(spyBotPanel)
+                .showConfirmDialog((Component) any(), anyString(), anyString(), anyInt());
+
+        spyBotPanel.getDuplicateBot().doClick();
+        verify(spyBotPanel, times(1)).duplicateAction();
+        verify(spyBotPanel, times(1)).showConfirmDialog((java.awt.Component) any(), anyString(), anyString(), anyInt());
+        //TODO: Test if actually duplicated
+    }
+
+    @Test
+    public void testDuplicateBotNo() {
+        doReturn(JOptionPane.NO_OPTION).when(spyBotPanel)
+                .showConfirmDialog((Component) any(), anyString(), anyString(), anyInt());
+
+        spyBotPanel.getDuplicateBot().doClick();
+        verify(spyBotPanel, times(1)).duplicateAction();
+        verify(spyBotPanel, times(1)).showConfirmDialog((java.awt.Component) any(), anyString(), anyString(), anyInt());
+
+        //TODO: Test if NOT duplicated
+    }
+
+    @Test
+    public void testDeleteBotYes() {
+        doReturn(JOptionPane.YES_OPTION).when(spyBotPanel)
+                .showConfirmDialog((Component) any(), anyString(), anyString(), anyInt());
+
+        spyBotPanel.getDeleteBot().doClick();
+        verify(spyBotPanel, times(1)).deleteAction();
+        verify(spyBotPanel, times(1)).showConfirmDialog((java.awt.Component) any(), anyString(), anyString(), anyInt());
+        //TODO: Test if actually deleted
+    }
+
+    @Test
+    public void testDeleteBotNo() {
+        doReturn(JOptionPane.NO_OPTION).when(spyBotPanel)
+                .showConfirmDialog((Component) any(), anyString(), anyString(), anyInt());
+
+        spyBotPanel.getDeleteBot().doClick();
+        verify(spyBotPanel, times(1)).deleteAction();
+        verify(spyBotPanel, times(1)).showConfirmDialog((java.awt.Component) any(), anyString(), anyString(), anyInt());
+
+        //TODO: Test if not deleted
+    }
+
+
+}
