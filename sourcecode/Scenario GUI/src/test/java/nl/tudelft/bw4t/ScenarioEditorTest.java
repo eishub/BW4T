70c878564126db0a4128d344109ab52b6f603668
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-05-15 14:48:07 +0200

    Fix for a invalid cast

--- sourcecode/Scenario GUI/src/test/java/nl/tudelft/bw4t/ScenarioEditorTest.java
+++ sourcecode/Scenario GUI/src/test/java/nl/tudelft/bw4t/ScenarioEditorTest.java
@@ -1,5 +1,8 @@
 package nl.tudelft.bw4t;
 
+import nl.tudelft.bw4t.gui.panel.BotPanel;
+import nl.tudelft.bw4t.gui.panel.ConfigurationPanel;
+import nl.tudelft.bw4t.gui.panel.MainPanel;
 import org.junit.Test;
 import javax.swing.*;
 import static org.junit.Assert.assertEquals;
@@ -15,7 +18,7 @@ public class ScenarioEditorTest {
     public void checkActivePane() {
         editor = new ScenarioEditor();
 
-        JPanel panel = new JPanel();
+        MainPanel panel = new MainPanel(new ConfigurationPanel(), new BotPanel());
 
         editor.setActivePane(panel);
         assertEquals(panel, editor.getActivePane());
