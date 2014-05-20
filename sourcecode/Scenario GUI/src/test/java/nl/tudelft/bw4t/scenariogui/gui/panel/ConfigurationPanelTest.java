d42d5458078308c09664199b96a9ebf2f5bf5e49
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-05-20 13:54:10 +0200

    Removed the invalid file test since it would require mocking a static method and reorganized the imports

--- sourcecode/Scenario GUI/src/test/java/nl/tudelft/bw4t/scenariogui/gui/panel/ConfigurationPanelTest.java
+++ sourcecode/Scenario GUI/src/test/java/nl/tudelft/bw4t/scenariogui/gui/panel/ConfigurationPanelTest.java
@@ -1,33 +1,41 @@
 package nl.tudelft.bw4t.scenariogui.gui.panel;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.io.File;
-
-import javax.swing.JFileChooser;
-
 import nl.tudelft.bw4t.scenariogui.ScenarioEditor;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.swing.*;
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created on 13/05/2014.
  */
 public class ConfigurationPanelTest {
 
-    /** The main GUI. */
+    /**
+     * The main GUI.
+     */
     private ScenarioEditor editor;
-    /** The file used to save the file in. */
+    /**
+     * The file used to save the file in.
+     */
     private final String filePath = "testpath.xml";
-    /** The file that has to be opened. */
+    /**
+     * The file that has to be opened.
+     */
     private final String filePathMap = "test.map";
-    /** The selected file that cannot be opened. */
+    /**
+     * The selected file that cannot be opened.
+     */
     private final String fileMapFail = "test.fail";
-    /** The file chooser object. */
+    /**
+     * The file chooser object.
+     */
     private JFileChooser fileChooser;
 
     /**
@@ -42,7 +50,7 @@ public class ConfigurationPanelTest {
 
         // Set the fileChooser used to the mocked one.
         editor.getMainPanel().getConfigurationPanel()
-            .setFileChooser(fileChooser);
+                .setFileChooser(fileChooser);
     }
 
     /**
@@ -79,27 +87,6 @@ public class ConfigurationPanelTest {
                 .doClick();
 
         assertEquals(filePathMap, editor.getMainPanel().getConfigurationPanel()
-                .getMapFile());
-    }
-
-    // Test the if clause, in case the return of JFileChooser is not
-    // APPROVE_OPTION
-
-    /**
-     * Tests the choosing of a map file (the file is not a MAP file).
-     */
-    @Test
-    public final void testMapFileActionFail() {
-        // Setup the mocks behaviour.
-        when(fileChooser.showOpenDialog(editor.getMainPanel())).thenReturn(
-                JFileChooser.APPROVE_OPTION);
-        when(fileChooser.getSelectedFile()).thenReturn(new File(fileMapFail));
-
-        // Trigger the event.
-        editor.getMainPanel().getConfigurationPanel().getChooseMapFile()
-                .doClick();
-
-        assertEquals("", editor.getMainPanel().getConfigurationPanel()
                 .getMapFile());
     }
 
