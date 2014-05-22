af377c3fb07897627992120e9df4bfd430115f47
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-05-22 12:00:17 +0200

    Checkstyle fixes.

--- sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/scenariogui/controller/NoMockOptionPrompt.java
+++ sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/scenariogui/controller/NoMockOptionPrompt.java
@@ -4,13 +4,22 @@ import java.awt.Component;
 
 import javax.swing.JOptionPane;
 
+/**
+ * The OptionPrompt that always returns no, used when mocking the
+ * objects during tests, to prevent the system from hanging due
+ * to having to press a button on the prompt.
+ */
 public class NoMockOptionPrompt implements OptionPrompt {
 
     /**
-     *  Returns the JOptionPane.NO_OPTION
-     *  @see javax.swing.JOptionPane#showConfirmDialog(java.awt.Component, Object, String, int, int);
+     * @see javax.swing.JOptionPane#showConfirmDialog(java.awt.Component, Object, String, int, int);
+     * @param parentComponent The <code>java.awt.Component</code> that will be used to align the box.
+     * @param message The message to be shown to the user.
+     * @param title The title of the confirmation dialog
+     * @param optionType The option type
+     * @param messageType The message type
+     * @return The chosen option
      */
-    @Override
     public int showConfirmDialog(Component parentComponent, Object message, String title,
                                  int optionType, int messageType) {
         return JOptionPane.NO_OPTION;
