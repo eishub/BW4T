ea22f208bc69c8b30d32c024d4f38e54070035c6
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-05-22 10:42:24 +0200

    Rename the class

---
+++ sourcecode/Scenario GUI/src/main/java/nl/tudelft/bw4t/scenariogui/controller/AbstractMenuOption.java
@@ -0,0 +1,168 @@
+package nl.tudelft.bw4t.scenariogui.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.swing.JFileChooser;
+import javax.xml.bind.JAXBException;
+
+import nl.tudelft.bw4t.scenariogui.ScenarioEditor;
+import nl.tudelft.bw4t.scenariogui.config.BW4TClientConfig;
+import nl.tudelft.bw4t.scenariogui.gui.MenuBar;
+import nl.tudelft.bw4t.scenariogui.gui.panel.MainPanel;
+import nl.tudelft.bw4t.scenariogui.util.FileFilters;
+
+/**
+ * Handles the event of the menu.
+ */
+public abstract class AbstractMenuOption implements ActionListener {
+
+    /**
+     * The menu bar as view.
+     */
+    private MenuBar view;
+    /**
+     * The current controller that is controlling this.
+     */
+    private Controller controller;
+
+    //made a variable for this so we can call it during testing
+    /**
+     * The file chooser.
+     */
+    private JFileChooser currentFileChooser;
+
+    /**
+     * The option prompt used.
+     */
+    private OptionPrompt optionPrompt;
+
+    /**
+     * Constructs a menu option object.
+     *
+     * @param newView  The new view.
+     * @param mainView The main view controller.
+     */
+    public AbstractMenuOption(final MenuBar newView, final Controller mainView) {
+        this.view = newView;
+        this.setController(mainView);
+
+        /* Set the intial file chooser and option prompt, can eventually be changed when tests
+         * need to be ran.
+         */
+        setCurrentFileChooser(new JFileChooser());
+        setOptionPrompt(new DefaultOptionPrompt());
+    }
+
+    /**
+     * Gets the current file chooser.
+     *
+     * @return The current file chooser.
+     */
+    public JFileChooser getCurrentFileChooser() {
+        return currentFileChooser;
+    }
+
+    /**
+     * Sets the new current file chooser.
+     *
+     * @param newFileChooser The new file chooser to set.
+     */
+    public void setCurrentFileChooser(final JFileChooser newFileChooser) {
+        currentFileChooser = newFileChooser;
+    }
+
+    /**
+     * Saves a file.
+     */
+    public void saveFile() {
+        saveFile(!view.hasLastFileLocation());
+    }
+
+    /**
+     * Saves a file.
+     *
+     * @param saveAs Whether or not to open a file chooser.
+     */
+    public void saveFile(final boolean saveAs) {
+        String path = view.getLastFileLocation();
+        if (saveAs || !view.hasLastFileLocation()) {
+            currentFileChooser = getCurrentFileChooser();
+
+            /** Adds an xml filter for the file chooser: */
+            currentFileChooser.setFileFilter(FileFilters.xmlFilter());
+
+            if (currentFileChooser.showSaveDialog(getController().getMainView()) == JFileChooser.APPROVE_OPTION) {
+                File file = currentFileChooser.getSelectedFile();
+
+                path = file.getAbsolutePath();
+
+                String extension = ".xml";
+                if (!path.endsWith(extension)) {
+                    path += extension;
+                }
+            }
+            else {
+                return;
+            }
+        }
+        try {
+            new BW4TClientConfig((MainPanel) (getController().getMainView()).getContentPane(), path).toXML();
+            view.setLastFileLocation(path);
+        } catch (JAXBException e) {
+            ScenarioEditor.handleException(
+                    e, "Error: Saving to XML has failed.");
+        } catch (FileNotFoundException e) {
+            ScenarioEditor.handleException(
+                    e, "Error: No file has been found.");
+        }
+    }
+
+    /**
+     * Gets called when the button associated with this action
+     * is pressed.
+     *
+     * @param e The action event.
+     */
+    public abstract void actionPerformed(ActionEvent e);
+
+    /**
+     * Gets the controller.
+     *
+     * @return The controller.
+     */
+    public Controller getController() {
+        return controller;
+    }
+
+    /**
+     * Sets the controller.
+     *
+     * @param newController The new controller.
+     */
+    public void setController(final Controller newController) {
+        controller = newController;
+    }
+
+    /**
+     * Gets the Option Prompt
+     *
+     * @return The <code>OptionPrompt</code>
+     */
+    public OptionPrompt getOptionPrompt() {
+        return optionPrompt;
+    }
+
+    /**
+     * Set the option prompt
+     *
+     * @param newOptionPrompt The <code>OptionPrompt</code> to be used.
+     */
+    public void setOptionPrompt(OptionPrompt newOptionPrompt) {
+        this.optionPrompt = newOptionPrompt;
+    }
+}
+
+
