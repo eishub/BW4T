c3f92e74fd36b7e1054d438e88a92d165f140531
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-05-23 19:42:54 +0200

    Added the disposing of the ScenarioEditor frame after the test and reformat some code.

--- bw4t/bw4t-scenario-editor/src/test/java/nl/tudelft/bw4t/scenariogui/gui/MenuBarTest.java
+++ bw4t/bw4t-scenario-editor/src/test/java/nl/tudelft/bw4t/scenariogui/gui/MenuBarTest.java
@@ -10,9 +10,10 @@ import javax.xml.bind.JAXBException;
 
 import nl.tudelft.bw4t.scenariogui.ScenarioEditor;
 import nl.tudelft.bw4t.scenariogui.config.BW4TClientConfig;
-import nl.tudelft.bw4t.scenariogui.controllers.menubar.AbstractMenuOption;
 import nl.tudelft.bw4t.scenariogui.controllers.NoMockOptionPrompt;
 import nl.tudelft.bw4t.scenariogui.controllers.YesMockOptionPrompt;
+import nl.tudelft.bw4t.scenariogui.controllers.menubar.AbstractMenuOption;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -35,19 +36,10 @@ import static org.mockito.Mockito.when;
 public class MenuBarTest {
 
     /**
-     * The Scenario editor that is spied upon for this test.
-     */
-    private ScenarioEditor editor;
-
-    /**
-     * File choose used to mock the behaviour of the user.
-     */
-    private JFileChooser filechooser;
-
-    /**
      * The base directory of all files used in the test.
      */
     private static final String BASE = System.getProperty("user.dir") + "/src/test/resources/";
+
     /**
      * The path of the xml file used to test the open button.
      */
@@ -57,6 +49,17 @@ public class MenuBarTest {
      * The path of the xml file used to save dummy data
      */
     private static final String FILE_SAVE_PATH = BASE + "dummy.xml";
+
+    /**
+     * The Scenario editor that is spied upon for this test.
+     */
+    private ScenarioEditor editor;
+
+    /**
+     * File choose used to mock the behaviour of the user.
+     */
+    private JFileChooser filechooser;
+
     /**
      * Setup the testing environment by creating the scenario editor and
      * assigning the editor attribute to a spy object of the ScenarioEditor.
@@ -77,11 +80,20 @@ public class MenuBarTest {
     }
 
     /**
+     * Close the ScenarioEditor to prevent to many windows from cluttering
+     * the screen during the running of the tests
+     */
+    @After
+    public final void closeEditor() {
+        editor.dispose();
+    }
+
+    /**
      * Test if the open button works.
      *
      * @throws FileNotFoundException File not found exception
-     * @throws JAXBException JAXBException, also called in some cases when a file is not found
-     * by JAXB itself.
+     * @throws JAXBException         JAXBException, also called in some cases when a file is not found
+     *                               by JAXB itself.
      */
     @Test
     public void testOpenButton() throws FileNotFoundException, JAXBException {
@@ -105,8 +117,8 @@ public class MenuBarTest {
      * Test if the open button works after changing the defaults and clicking yes on the prompt.
      *
      * @throws FileNotFoundException File not found exception
-     * @throws JAXBException JAXBException, also called in some cases when a file is not found
-     * by JAXB itself.
+     * @throws JAXBException         JAXBException, also called in some cases when a file is not found
+     *                               by JAXB itself.
      */
     @Test
     public void testOpenButtonNonDefaultYes() throws FileNotFoundException, JAXBException {
@@ -153,8 +165,8 @@ public class MenuBarTest {
      * Test if the open button works after changing the defaults and clicking yes on the prompt.
      *
      * @throws FileNotFoundException File not found exception
-     * @throws JAXBException JAXBException, also called in some cases when a file is not found
-     * by JAXB itself.
+     * @throws JAXBException         JAXBException, also called in some cases when a file is not found
+     *                               by JAXB itself.
      */
     @Test
     public void testOpenButtonNonDefaultNo() throws FileNotFoundException, JAXBException {
