967473fbff75cef4d211717f4a706ff447ef0538
Jan Giesenberg <giesenja@gmail.com>
2014-05-24 00:10:04 +0200

    removing unecessary ;
    Former-commit-id: fc862ee9356d934a9115e2c1bc9536c78ddc08a3 [formerly a1abb18801d24177620a02318668d0f6832b3164]
    Former-commit-id: 619bff14054389367abb393a6e63473d8857f304

--- bw4t/bw4t-core/src/main/java/nl/tudelft/bw4t/map/Door.java
+++ bw4t/bw4t-core/src/main/java/nl/tudelft/bw4t/map/Door.java
@@ -13,7 +13,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Door implements Serializable {
     public enum Orientation {
         HORIZONTAL, VERTICAL
-    };
+    }
 
     private Point position = new Point();
     private Orientation orientation = Orientation.HORIZONTAL;
