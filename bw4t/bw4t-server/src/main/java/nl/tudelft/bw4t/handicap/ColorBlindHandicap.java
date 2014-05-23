ee26c2bb2176d8144a697ec97ff26e1ccfa88979
Martin Rogalla <M.J.Rogalla@student.tudelft.nl>
2014-05-23 11:16:36 +0200

    Merged Handicap from botstoreGUI
    Former-commit-id: 356df7e9d0665e1c1ca0bc1d5745bfa549529012

---
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/handicap/ColorBlindHandicap.java
@@ -0,0 +1,39 @@
+package nl.tudelft.bw4t.handicap;
+/**
+ * 
+ * @author Ruben Starmans
+ *
+ */
+public class ColorBlindHandicap extends Handicap{
+	
+	public boolean isActive;
+	
+	/**
+	 * Sets the handicap to active,
+	 * Adds the handicap to the robot handicap storage.
+	 * @param p is the HandicapInterface the ColorBlindHandicap wraps around.
+	 */
+	public ColorBlindHandicap(HandicapInterface p) {
+		super(p);
+		isActive = true;
+		robot.handicapsMap.put("ColorBlind", this);
+	}
+	
+	/**
+	 * Activate the handicap.
+	 */
+	public void activate()
+	{
+		isActive = true;
+	}
+	
+	/**
+	 * Deactivate the handicap.
+	 */
+	public void deactivate()
+	{
+		isActive = false;
+	}
+
+	
+}
---
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/handicap/MoveSpeedHandicap.java
@@ -0,0 +1,95 @@
+package nl.tudelft.bw4t.handicap;
+
+import nl.tudelft.bw4t.robots.Robot;
+import nl.tudelft.bw4t.server.BW4TLogger;
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.space.SpatialException;
+import repast.simphony.space.SpatialMath;
+/**
+ * 
+ * @author Valentine Mairet & Ruben Starmans
+ *
+ */
+public class MoveSpeedHandicap extends Handicap {
+	/**
+	 * modifier for the distance per tick
+	 */
+	private double speedMod;
+	/**
+	 * boolean value to check if the handicap is active or not
+	 */
+	public boolean isActive;
+	/**
+	 * Constructor for the handicap
+	 * @param p HandicapInterface of the MoveSpeedHandicap Wraps around
+	 * @param speedModifier modifier fo the distance per tick
+	 */
+	public MoveSpeedHandicap(HandicapInterface p, double speedModifier) {
+		super(p);
+		speedMod = speedModifier;
+		isActive = true;
+		
+		robot.handicapsMap.put("MoveSpeed", this);
+	}
+	/**
+	 * Overridden method move of Robot which has the speed modifier
+	 */
+	@ScheduledMethod(start = 0, duration = 0, interval = 1)
+	public synchronized void move() {
+		if (isActive) {
+			if (robot.targetLocation != null) {
+				// Calculate the distance that the robot is allowed to move.
+				double distance = robot.distanceTo(robot.targetLocation);
+				if (distance < Robot.MIN_MOVE_DISTANCE) {
+					robot.stopRobot(); // we're there
+				} else {
+					double movingDistance = Math.min(distance, Robot.MAX_MOVE_DISTANCE * speedMod);
+		
+					// Angle at which to move
+					double angle = SpatialMath.calcAngleFor2DMovement(robot.space,
+							robot.getLocation(), robot.targetLocation);
+		
+					// The displacement of the robot
+					double[] displacement = SpatialMath.getDisplacement(2, 0,
+							movingDistance, angle);
+		
+					try {
+						// Move the robot to the new position using the displacement
+						robot.moveByDisplacement(displacement[0], displacement[1]);
+						BW4TLogger.getInstance().logMoving(robot.name);
+						
+						/**
+						 * Valentine
+						 * The robot's battery discharges when it moves.
+						 */
+						robot.battery.discharge();
+					} catch (SpatialException e) {
+						robot.collided = true;
+						robot.stopRobot();
+					}
+				}
+			}
+		}
+		else {
+			super.move();
+		}
+	}
+	/**
+	 * set the handicap to active
+	 */
+	public void activate()
+	{
+		isActive = true;
+	}
+	/**
+	 * set the handicap to inactive
+	 */
+	public void deactivate()
+	{
+		isActive = false;
+	}
+	
+	public double getSpeedMod() {
+		return speedMod;
+	}
+}
