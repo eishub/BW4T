37fea7006f8d37362ae37e72f46a9ca722d8d4d8
Joop Aué <JoopAue@gmail.com>
2014-05-25 14:32:05 +0200

    Changed default names of bot and epartner to have a counter
    Former-commit-id: 92136bc07dbf6d58f5625816a14e67ca64db0d76 [formerly 5354dcc45785dc6d9baa8d3093ff19e84e5008c5]
    Former-commit-id: 950ece19c693df9cd12737823ea854bdf0ff2a3d

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/editor/AddNewEPartner.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/editor/AddNewEPartner.java
@@ -18,7 +18,12 @@ class AddNewEPartner implements ActionListener {
      * The <code>MainPanel</code> serving as the content pane.
      */
     private MainPanel view;
-
+    
+    /**
+     * Keeps track of the amount of e-Partners created
+     */
+    private int eCount;
+    
     /**
      * Create an AddNewEpartner event handler.
      *
@@ -31,11 +36,13 @@ class AddNewEPartner implements ActionListener {
     /**
      * Executes action that needs to happen when the "New E-partner" button is
      * pressed.
-     *
+     * Gives default name of "E-partner \<n\>" where \<n\> is the n'th e-Parnter created.
+     *   
      * @param ae The action
      */
     public void actionPerformed(ActionEvent ae) {
-        Object[] newEPartnerObject = {"Unnamed E-partner", 1};
+        eCount++;
+        Object[] newEPartnerObject = {"E-partner" + eCount, 1};
         view.getEntityPanel().getEPartnerTableModel().addRow(newEPartnerObject);
     }
 }
