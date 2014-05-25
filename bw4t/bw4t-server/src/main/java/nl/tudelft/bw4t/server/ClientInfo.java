eishub/BW4T

commit 3cb0649df1f99f326fefb42afef24b66bbb41bad

moved environment and launcher into seperate package, cleaned up the BW4TServer a bit

bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/ClientInfo.java
@@ -1,0 +1,37 @@
+package nl.tudelft.bw4t.server;
+
+class ClientInfo {
+	private int numberOfHumans = 0;
+	private int numberOfAgents = 0;
+	
+	public ClientInfo(int reqHuman, int reqAgent) {
+		assert reqHuman >= 0;
+		assert reqAgent >= 0;
+		this.numberOfAgents = reqAgent;
+		this.numberOfHumans = reqAgent;
+	}
+
+	public int getNumberOfHumans() {
+		return numberOfHumans;
+	}
+
+	public boolean decreaseNumberOfHumans() {
+		if (numberOfHumans > 0) {
+			this.numberOfHumans--;
+			return true;
+		}
+		return false;
+	}
+
+	public int getNumberOfAgents() {
+		return numberOfAgents;
+	}
+
+	public boolean decreaseNumberOfAgents() {
+		if (this.numberOfAgents > 0) {
+			this.numberOfAgents--;
+			return true;
+		}
+		return false;
+	}
+}

bw4t/bw4t-integration-test/pom.xml
@@ -49,9 +49,9 @@
 							<target>
 								<mkdir dir="target/run-server/" />
 								<java fork="true" spawn="true" dir="target/run-server/"
-									classpathref="maven.runtime.classpath" className="nl.tudelft.bw4t.server.Launcher"
+									classpathref="maven.runtime.classpath" className="nl.tudelft.bw4t.server.environment.Launcher"
 									args="-gui false -key bw4tServerIntegration" />
-								<waitfor maxwait="3" maxwaitunit="minute" checkevery="200">
+								<waitfor maxwait="1" maxwaitunit="minute" checkevery="200">
 									<socket server="localhost" port="8000" />
 								</waitfor>
 								<sleep seconds="5" />

bw4t/bw4t-integration-test/src/test/java/nl/tudelft/bw4t/client/MovementTest.java
@@ -12,7 +12,7 @@ import javax.xml.bind.JAXBException;
 
 import nl.tudelft.bw4t.client.environment.RemoteEnvironment;
 import nl.tudelft.bw4t.client.environment.handlers.PerceptsHandler;
-import nl.tudelft.bw4t.server.BW4TEnvironment;
+import nl.tudelft.bw4t.server.environment.BW4TEnvironment;
 
 import org.junit.After;
 import org.junit.Before;

bw4t/bw4t-server/bw4t-server.launch
@@ -1,14 +1,14 @@
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <launchConfiguration type="org.eclipse.jdt.launching.localJavaApplication">
 <listAttribute key="org.eclipse.debug.core.MAPPED_RESOURCE_PATHS">
-<listEntry value="/bw4t-server/src/main/java/nl/tudelft/bw4t/server/Launcher.java"/>
+<listEntry value="/bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/Launcher.java"/>
 </listAttribute>
 <listAttribute key="org.eclipse.debug.core.MAPPED_RESOURCE_TYPES">
 <listEntry value="1"/>
 </listAttribute>
 <booleanAttribute key="org.eclipse.jdt.launching.ATTR_USE_START_ON_FIRST_THREAD" value="true"/>
 <stringAttribute key="org.eclipse.jdt.launching.CLASSPATH_PROVIDER" value="org.eclipse.m2e.launchconfig.classpathProvider"/>
-<stringAttribute key="org.eclipse.jdt.launching.MAIN_TYPE" value="nl.tudelft.bw4t.server.Launcher"/>
+<stringAttribute key="org.eclipse.jdt.launching.MAIN_TYPE" value="nl.tudelft.bw4t.server.environment.Launcher"/>
 <stringAttribute key="org.eclipse.jdt.launching.PROJECT_ATTR" value="bw4t-server"/>
 <stringAttribute key="org.eclipse.jdt.launching.SOURCE_PATH_PROVIDER" value="org.eclipse.m2e.launchconfig.sourcepathProvider"/>
 <stringAttribute key="org.eclipse.jdt.launching.WORKING_DIRECTORY" value="${workspace_loc:bw4t-server/target}"/>

bw4t/bw4t-server/pom.xml
@@ -27,7 +27,7 @@
 				<configuration>
 					<archive>
 						<manifest>
-							<mainClass>nl.tudelft.bw4t.server.Launcher</mainClass>
+							<mainClass>nl.tudelft.bw4t.server.environment.Launcher</mainClass>
 						</manifest>
 					</archive>
 					<descriptorRefs>

bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/BW4TBuilder.java
@@ -6,9 +6,9 @@ import javax.xml.bind.JAXBException;
 
 import org.apache.log4j.Logger;
 
-import nl.tudelft.bw4t.server.BW4TEnvironment;
-import nl.tudelft.bw4t.server.Launcher;
-import nl.tudelft.bw4t.server.Stepper;
+import nl.tudelft.bw4t.server.environment.BW4TEnvironment;
+import nl.tudelft.bw4t.server.environment.Launcher;
+import nl.tudelft.bw4t.server.environment.Stepper;
 import repast.simphony.context.Context;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.engine.environment.RunListener;

bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/MapLoader.java
@@ -23,8 +23,8 @@ import nl.tudelft.bw4t.map.NewMap;
 import nl.tudelft.bw4t.map.Zone;
 import nl.tudelft.bw4t.robots.NavigatingRobot;
 import nl.tudelft.bw4t.robots.Robot;
-import nl.tudelft.bw4t.server.BW4TEnvironment;
-import nl.tudelft.bw4t.server.BW4TLogger;
+import nl.tudelft.bw4t.server.environment.BW4TEnvironment;
+import nl.tudelft.bw4t.server.logging.BW4TLogger;
 import nl.tudelft.bw4t.zone.BlocksRoom;
 import nl.tudelft.bw4t.zone.Corridor;
 import nl.tudelft.bw4t.zone.DropZone;

bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/eis/RobotEntity.java
@@ -16,10 +16,10 @@ import nl.tudelft.bw4t.eis.translators.PointTranslator;
 import nl.tudelft.bw4t.eis.translators.ZoneTranslator;
 import nl.tudelft.bw4t.robots.NavigatingRobot;
 import nl.tudelft.bw4t.robots.Robot;
-import nl.tudelft.bw4t.server.BW4TEnvironment;
-import nl.tudelft.bw4t.server.BW4TLogger;
-import nl.tudelft.bw4t.server.Launcher;
 import nl.tudelft.bw4t.server.RobotEntityInt;
+import nl.tudelft.bw4t.server.environment.BW4TEnvironment;
+import nl.tudelft.bw4t.server.environment.Launcher;
+import nl.tudelft.bw4t.server.logging.BW4TLogger;
 import nl.tudelft.bw4t.util.RoomLocator;
 import nl.tudelft.bw4t.util.ZoneLocator;
 import nl.tudelft.bw4t.zone.BlocksRoom;

commit 12a3cbf3b2fc9b0e4b14dc250700990ab4cb649e

fixed the startup of the human agents caused in commit 3cb0649df1f99f326fefb42afef24b66bbb41bad

bw4t/.classpath
@@ -1,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<classpath>
+	<classpathentry kind="src" path="src/main/java"/>
+	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER"/>
+	<classpathentry kind="output" path="target"/>
+</classpath>

bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/ClientInfo.java
@@ -3,12 +3,12 @@ package nl.tudelft.bw4t.server;
 class ClientInfo {
 	private int numberOfHumans = 0;
 	private int numberOfAgents = 0;
-	
+
 	public ClientInfo(int reqHuman, int reqAgent) {
 		assert reqHuman >= 0;
 		assert reqAgent >= 0;
 		this.numberOfAgents = reqAgent;
-		this.numberOfHumans = reqAgent;
+		this.numberOfHumans = reqHuman;
 	}
 
 	public int getNumberOfHumans() {