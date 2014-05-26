edce4e2d64be18775fc3401e4a3bd05a1a0711db
Jan Giesenberg <giesenja@gmail.com>
2014-05-26 02:25:53 +0200

    MapRenderer now adjusts its size properly
    Former-commit-id: 685de5f58923cb7df1ef3515ca0a96128144df0a [formerly 9eaf70efbc4521d55a8d3b4de88db95544b3b839]
    Former-commit-id: 3a6e5d4857a945958adfac347f37f45094e0ca7e

--- bw4t/bw4t-core/src/test/java/nl/tudelft/bw4t/view/MapRendererTest.java
+++ bw4t/bw4t-core/src/test/java/nl/tudelft/bw4t/view/MapRendererTest.java
@@ -3,6 +3,7 @@ package nl.tudelft.bw4t.view;
 import java.io.InputStream;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.xml.bind.JAXBException;
 
 import nl.tudelft.bw4t.controller.MapController;
@@ -11,32 +12,31 @@ import nl.tudelft.bw4t.map.NewMap;
 
 public class MapRendererTest extends JFrame {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 3165062664023548124L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3165062664023548124L;
 
-    MapRenderer mapRenderer;
-    NewMap map;
-    MapController mapController;
+	MapRenderer mapRenderer;
+	NewMap map;
+	MapController mapController;
 
-    public MapRendererTest() throws JAXBException {
-        InputStream inputStream = MapRendererTest.class.getResourceAsStream("/Rainbow");
-        map = NewMap.create(inputStream);
-        mapController = new TestMapController(map);
-        mapRenderer = new MapRenderer(mapController);
-        this.add(mapRenderer);
-        this.pack();
-    }
+	public MapRendererTest() throws JAXBException {
+		InputStream inputStream = MapRendererTest.class.getResourceAsStream("/Rainbow");
+		map = NewMap.create(inputStream);
+		mapController = new TestMapController(map);
+		mapRenderer = new MapRenderer(mapController);
+		this.add(new JScrollPane(mapRenderer));
+		this.pack();
+	}
 
-    public static void main(String[] args) {
-        try {
-            JFrame testing = new MapRendererTest();
-            testing.setVisible(true);
-            testing.setBounds(0, 0, 350, 600);
-            testing.setLocationRelativeTo(null);
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			JFrame testing = new MapRendererTest();
+			testing.setVisible(true);
+			testing.setLocationRelativeTo(null);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
 }
