090a10a5d223af56a93cc51cc1baf7f4867ccb48
Ruben <rubenstarmans@gmail.com>
2014-05-26 10:04:49 +0200

    Updated E-Partner
    Changed constructor

--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/blocks/EPartner.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/blocks/EPartner.java
@@ -9,11 +9,6 @@ import nl.tudelft.bw4t.robots.Robot;
  * @author Valentine Mairet & Ruben Starmans
  */
 public class EPartner extends BoundedMoveableObject{
-
-	/**
-	 * type of the E-Partner
-	 */
-	private String eType;
 	
 	/**
 	 * Robot that picked up the E-Partner
@@ -25,18 +20,14 @@ public class EPartner extends BoundedMoveableObject{
 	 * @param space
 	 * @param context
 	 */
-	public EPartner(String type, ContinuousSpace<Object> space, Context<Object> context) {
+	public EPartner(ContinuousSpace<Object> space, Context<Object> context) {
 		super(space, context);
-		eType = type;
 	}
-	
-	
-	
 	/**
 	 * @param holder Robot that picked up the E-Partner
 	 */
-	public void setHolder(Robot hold){
-		this.holder = hold;
+	public void setHolder(Robot robot){
+		this.holder = robot;
 	}
 	
 }
