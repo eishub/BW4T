6477e9c35ec284e8ff7f45c5bfe7fff05f9f25df
Nick <gnick.com@gmail.com>
2014-05-26 14:57:22 +0200

    Checkstyle.

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/editor/AbstractMenuOption.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/editor/AbstractMenuOption.java
@@ -8,7 +8,7 @@ import java.io.FileNotFoundException;
 import javax.swing.JFileChooser;
 import javax.xml.bind.JAXBException;
 
-import nl.tudelft.bw4t.scenarioeditor.BW4TClientConfig;
+import nl.tudelft.bw4t.scenariogui.BW4TClientConfig;
 import nl.tudelft.bw4t.scenariogui.ScenarioEditor;
 import nl.tudelft.bw4t.scenariogui.config.BW4TClientConfigIntegration;
 import nl.tudelft.bw4t.scenariogui.gui.MenuBar;
