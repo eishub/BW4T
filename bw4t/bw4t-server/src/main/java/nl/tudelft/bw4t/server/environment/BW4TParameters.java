f4c552462edd339d9cd9f6ccdc1bdfe49f307e0e
Daniel Swaab <swaab.daniel@gmail.com>
2014-05-27 13:58:11 +0200

    fixed issues in parameters class
    Former-commit-id: 89566084873a32dc4d8c0152fa66f5efa78cdeb8 [formerly 3c6a655b554c64510f782557ab80b37bc872cfd6]
    Former-commit-id: 0025faf9b3008dc6ef808e2d683c96edd0d6ff80

--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/BW4TParameters.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/BW4TParameters.java
@@ -6,47 +6,39 @@ import repast.simphony.parameter.Schema;
 public class BW4TParameters implements Parameters  {
 
 	public BW4TParameters() {
-		// TODO Auto-generated constructor stub
 	}
 	@Override
 	public void setValue(String paramName, Object val) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public boolean isReadOnly(String paramName) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public String getValueAsString(String paramName) {
-		// TODO Auto-generated method stub
 		return "asd";
 	}
 
 	@Override
 	public Object getValue(String paramName) {
-		// TODO Auto-generated method stub
 		return 223;
 	}
 
 	@Override
 	public Schema getSchema() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getDisplayName(String paramName) {
-		// TODO Auto-generated method stub
 		return "asd";
 	}
 
 	@Override
 	public Parameters clone() {
-		// TODO Auto-generated method stub
 		return this;
 	}
 }
