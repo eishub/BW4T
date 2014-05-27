6a78ddf2b11d874b76396e641fbacc04bb017dcf
Arun Malhoe <arunmalhoe@gmail.com>
2014-05-27 09:55:57 +0200

    move functionality from panel to respective slider
    Former-commit-id: 5fdc9864e9de096e5938661316033bbf9cca5893 [formerly 2a661de943e69c77cd44f3762e90fd5c2851d05f]
    Former-commit-id: 31f48377b47f8b4ddf493aa5d34c1840765bade1

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/botstore/SizeSlider.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/botstore/SizeSlider.java
@@ -2,6 +2,7 @@ package nl.tudelft.bw4t.scenariogui.controllers.botstore;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.text.DecimalFormat;
 
 import nl.tudelft.bw4t.scenariogui.gui.botstore.BotEditorPanel;
 
@@ -33,8 +34,28 @@ class SizeSlider implements MouseListener {
 
     @Override
     public void mouseReleased(MouseEvent arg0) {
-        view.calculateBatteryUse();
+        int speed = view.getSpeedSlider().getValue();
+        int size = view.getSizeSlider().getValue();
+        double res = 0.002*size + 0.000025*speed;
+        DecimalFormat df = new DecimalFormat("#.######");
+        String value = df.format(res);
+        view.getBatteryUseValueLabel().setText(padString(value));
 
     }
-
+    
+	/**
+	 * Pad the string with zeros (the string with
+	 * the value for the battery usage is aligned with
+	 * the sliders, and will cause the sliders to resize
+	 * when changed. This function keeps the string at a
+	 * certain length, so the sliders aren't resized anymore).
+	 * @param value The string to be padded.
+	 * @return The padded string.
+	 */
+	public String padString(String value) {
+		while(value.length() < 8) {
+			value += "0";
+		}
+		return value;
+	}
 }
