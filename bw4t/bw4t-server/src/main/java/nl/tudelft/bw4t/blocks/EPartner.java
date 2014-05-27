810bed2d92272451eb5606d25e376a77416e8a99
Daniel Swaab <swaab.daniel@gmail.com>
2014-05-27 14:08:46 +0200

    fixed some issues with tabs spacing
    Former-commit-id: e3b913f52f406189ab17d20265b28a8ce28c3762 [formerly d4395cd3fdc97cbf2c2190f6d69c3503ea7c9f57]
    Former-commit-id: 22618b822859c50755ceadd073faa20b3b215434

--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/blocks/EPartner.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/blocks/EPartner.java
@@ -8,20 +8,22 @@ import nl.tudelft.bw4t.robots.Robot;
 /**
  * @author Valentine Mairet & Ruben Starmans
  */
-public class EPartner extends BoundedMoveableObject{
+public class EPartner extends BoundedMoveableObject {
 
 	/**
 	 * type of the E-Partner
 	 */
 	private String eType;
-	
+
 	/**
 	 * Robot that picked up the E-Partner
 	 */
 	private Robot holder;
+
 	/**
 	 * 
-	 * @param type Type of E-Partner
+	 * @param type
+	 *            Type of E-Partner
 	 * @param space
 	 * @param context
 	 */
@@ -29,14 +31,13 @@ public class EPartner extends BoundedMoveableObject{
 		super(space, context);
 		eType = type;
 	}
-	
-	
-	
+
 	/**
-	 * @param holder Robot that picked up the E-Partner
+	 * @param holder
+	 *            Robot that picked up the E-Partner
 	 */
-	public void setHolder(Robot hold){
+	public void setHolder(Robot hold) {
 		this.holder = hold;
 	}
-	
+
 }
