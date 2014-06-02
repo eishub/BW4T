69f2578c4a31f59a0e93ebaef6c3882645bea666
Nick <gnick.com@gmail.com>
2014-06-02 13:49:26 +0200

    Updates the launcher file (for the batchrunner) with possible solutions.
    Former-commit-id: 2d8d47c2df0dd6a3b7466411b15ec98f216049a1 [formerly 6613d4be2e915273e4d062ab16bbe8f046933935]
    Former-commit-id: 2ba40cafeff02efe50390678a8cadc745cbc7e98

--- bw4t/bw4t-batchrunner/src/main/java/nl/tudelft/bw4t/batchrunner/Launcher.java
+++ bw4t/bw4t-batchrunner/src/main/java/nl/tudelft/bw4t/batchrunner/Launcher.java
@@ -1,10 +1,48 @@
 package nl.tudelft.bw4t.batchrunner;
 
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import goal.core.mas.MASProgram;
+import goal.tools.BatchRun;
+import goal.tools.Run;
+import goal.tools.SingleRun;
+import goal.tools.errorhandling.exceptions.GOALParseException;
+
 public class Launcher {
     
     public static void main(String[] args) {
-        // import Run from goal.jar
+        
+        
+        //ok, so the Run object was supposed to work as below:
         //new Run(new File("MAS File Loc Here")).run();
+        
+        //but apparently the GOAL code has changed, so below are some attempts:
+//        try {
+//            Run.run("what", "are", "these?");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        
+//        //possibly use BatchRun object instead of Run object (just guess work, we need some documentation):
+//        @SuppressWarnings("unused")
+//        MASProgram p = new MASProgram(null, null);
+//        try {
+//            new BatchRun((List<MASProgram>) new LinkedList<MASProgram>()).run();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        
+//        //another method/attempt suggested by Wouter:
+//        try {
+//            @SuppressWarnings("unused")
+//            SingleRun run = new SingleRun(new File("MAS File Loc Here"));
+//        } catch (GOALParseException e) {
+//            e.printStackTrace();
+//        }
+        
     }
 
 }
