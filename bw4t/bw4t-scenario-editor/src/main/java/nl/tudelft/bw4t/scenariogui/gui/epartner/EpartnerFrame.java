675eacbb0c5c30a2fd5166674c847702ca1964e7
Arun Malhoe <arunmalhoe@gmail.com>
2014-06-02 12:49:02 +0200

    checkstyled!
    Former-commit-id: 6b3603cbe92682d496aedcb729e94d0160404efb [formerly 3adace1018ba30051913632d30f94d159608b8ea]
    Former-commit-id: a1ee858ac0bde2a845454bee25f62dd540898791

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/gui/epartner/EpartnerFrame.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/gui/epartner/EpartnerFrame.java
@@ -1,9 +1,6 @@
 package nl.tudelft.bw4t.scenariogui.gui.epartner;
 
 import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
--- bw4t/bw4t-scenario-editor/src/test/java/nl/tudelft/bw4t/botstore/boteditor/BotEditorTest.java
+++ bw4t/bw4t-scenario-editor/src/test/java/nl/tudelft/bw4t/botstore/boteditor/BotEditorTest.java
@@ -1,23 +1,30 @@
 package nl.tudelft.bw4t.botstore.boteditor;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 import nl.tudelft.bw4t.scenariogui.gui.botstore.BotEditor;
 import nl.tudelft.bw4t.scenariogui.gui.botstore.BotEditorPanel;
 import nl.tudelft.bw4t.scenariogui.gui.panel.ConfigurationPanel;
 import nl.tudelft.bw4t.scenariogui.gui.panel.EntityPanel;
 import nl.tudelft.bw4t.scenariogui.gui.panel.MainPanel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test for boteditor
+ * @author Arun
+ *
+ */
 public class BotEditorTest {
     
+	/** The boteditor used to test */
     private BotEditor botEditor;
     
+    /** Test the setup of boteditor */
     @Test
-    public void testBotEditorPane(){
-        MainPanel parent = new MainPanel(new ConfigurationPanel(),new EntityPanel());
-        botEditor = new BotEditor(parent,"");
+    public void testBotEditorPane() {
+        MainPanel parent = new MainPanel(new ConfigurationPanel(), new EntityPanel());
+        botEditor = new BotEditor(parent, "");
         BotEditorPanel panel = new BotEditorPanel("");
         botEditor.setBotEditorPanel(panel);
         botEditor.setParent(parent);
