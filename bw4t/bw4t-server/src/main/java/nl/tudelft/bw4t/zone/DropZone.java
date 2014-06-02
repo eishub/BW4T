3fd6e8ef61d06c983b9a77ab781aece8776ee508
Daniel Swaab <swaab.daniel@gmail.com>
2014-06-02 12:37:13 +0200

    changed a string-loop issues as test
    Former-commit-id: 22d30b5e9c747686d3cc8dd1295c13a836c1a080 [formerly e2c5202f7077d0d494a889b75bb6281a4ddebf01]
    Former-commit-id: b1924f1b6539705aabefd0ce23d5e605528acaf8

--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/zone/DropZone.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/zone/DropZone.java
@@ -61,12 +61,12 @@ public class DropZone extends Room {
     public void setSequence(List<BlockColor> colors) {
         sequence = colors;
         
-        String message = "sequence";
-        
+        StringBuffer s = new StringBuffer();
+        s.append("sequence");
         for (BlockColor col : sequence) {
-            message = message + " " + col.getLetter();
+            s.append(" " + col.getLetter());
         }
-                
+        String message = s.toString();        
         LOGGER.log(BotLog.BOTLOG, message);
         
     }
