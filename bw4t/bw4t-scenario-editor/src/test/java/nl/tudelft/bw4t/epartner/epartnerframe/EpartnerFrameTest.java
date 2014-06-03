477c68b2814cb4c5202d547ee39df042867694c2
Tim van Rossum <trvanrossum@gmail.com>
2014-06-03 09:41:15 +0200

    Changed some files to be compliant with checkstyle issues for the most part.
    Former-commit-id: 8acf65ab8b82908921b110e2466af763adf8f0a2 [formerly cb4ad51e4d34e42d75f132f086b3d5cc6862503b]
    Former-commit-id: 0ffe344401eba8d1a53ce0b425a6c3e146394497

--- bw4t/bw4t-scenario-editor/src/test/java/nl/tudelft/bw4t/epartner/epartnerframe/EpartnerFrameTest.java
+++ bw4t/bw4t-scenario-editor/src/test/java/nl/tudelft/bw4t/epartner/epartnerframe/EpartnerFrameTest.java
@@ -1,5 +1,7 @@
 package nl.tudelft.bw4t.epartner.epartnerframe;
 
+import nl.tudelft.bw4t.scenariogui.gui.epartner.EpartnerFrame;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,30 +9,46 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.spy;
-import nl.tudelft.bw4t.scenariogui.gui.epartner.EpartnerFrame;
-
+/**
+ * Test for the ePartner frame.
+ * @author Tim
+ *
+ */
 public class EpartnerFrameTest {
-	
+	/**
+	 * A normal frame.
+	 */
 	private EpartnerFrame frame;
+	/**
+	 * A spy frame.
+	 */
 	private EpartnerFrame spyframe;
-	
+	/**
+	 * Set up both frames.
+	 */
 	@Before
 	public final void setupEpartnerFrame() {
 		frame = new EpartnerFrame();
 		spyframe = spy(frame);
 	}
-	
+	/**
+	 * Delete the real frame.
+	 */
 	@After
-	public final void dispose(){
+	public final void dispose() {
 		frame.dispose();
 	}
-	
+	/**
+	 * Test the initial settings.
+	 */
 	@Test
 	public final void testInititalSetup() {
 		assertFalse(spyframe.getLeftAloneCheckbox().isSelected());
 		assertFalse(spyframe.getGPSCheckbox().isSelected());
 	}
-	
+	/**
+	 * Test selection of check boxes.
+	 */
 	@Test
 	public final void testModifyCheckBoxes() {
 		spyframe.getLeftAloneCheckbox().setSelected(true);
@@ -38,7 +56,9 @@ public class EpartnerFrameTest {
 		assertTrue(spyframe.getLeftAloneCheckbox().isSelected());
 		assertTrue(spyframe.getGPSCheckbox().isSelected());
 	}
-	
+	/**
+	 * Test the reset button functionality.
+	 */
 	@Test
 	public final void testResetButton() {
 		spyframe.getResetButton().doClick();
