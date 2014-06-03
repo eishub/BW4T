0f152083b8ae9ac7854ea2f5d3d717576732a654
Shirley de Wit <SdeWit@192.168.1.22>
2014-06-03 20:19:11 +0200

    Change layout of logfile
    Former-commit-id: f93f54635e559dd6a9b20a320242284a25d382e6 [formerly 89473398a8ec0dbda27850535351ef775589c75f]
    Former-commit-id: 0acf3a85214f999edbe924d996c5710f31ed8a10

--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/BW4TEnvironment.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/BW4TEnvironment.java
@@ -327,7 +327,7 @@ public class BW4TEnvironment extends AbstractEnvironment {
      */
     public Percept performClientAction(String entity, Action action) throws ActException {
         Long time = System.currentTimeMillis();
-        LOGGER.log(BotLog.BOTLOG, String.format("action %d %s %s", time, entity, action.toProlog()));
+        LOGGER.log(BotLog.BOTLOG, String.format("action %s %s", entity, action.toProlog()));
 
         if (starttime == null) {
             starttime = time;
