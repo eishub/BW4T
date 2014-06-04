e575b85049b5fc20756a4725bcfc6fadc77bf8d5
Katia Asmoredjo <katia.asmoredjo@gmail.com>
2014-06-04 16:19:40 +0200

    The epartner list updates when something in epartnerstore is changed
    Former-commit-id: ef5ffe4635f0b159cd6d64dadc093bbf1ce47660 [formerly b7827cb6369344208a22fc25e526b7ab7ff125eb]
    Former-commit-id: e9d34f8f42a82fb98978c39852bd2ad1b90b0373

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/epartner/EpartnerSaveButton.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/epartner/EpartnerSaveButton.java
@@ -3,6 +3,8 @@ package nl.tudelft.bw4t.scenariogui.controllers.epartner;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import nl.tudelft.bw4t.scenariogui.BotConfig;
+import nl.tudelft.bw4t.scenariogui.EPartnerConfig;
 import nl.tudelft.bw4t.scenariogui.gui.epartner.EpartnerFrame;
 import nl.tudelft.bw4t.scenariogui.gui.panel.MainPanel;
 
@@ -39,6 +41,26 @@ class EpartnerSaveButton implements ActionListener {
 		view.getDataObject().setForgetMeNot(
 				view.getLeftAloneCheckbox().isSelected());
 		view.getDataObject().setGps(view.getGPSCheckbox().isSelected());
+
+		updateEpartnerTable();
+
 		view.dispose();
+	}
+
+	/**
+	 * Updates the epartner list in the scenario editor.
+	 */
+	private void updateEpartnerTable() {
+		view.getPanel().getEntityPanel().getEPartnerTableModel().setRowCount(0);
+		int rows = view.getPanel().getEntityPanel().getEPartnerConfigs().size();
+
+		for (int i = 0; i < rows; i++) {
+			EPartnerConfig epartnerConfig = view.getPanel().getEntityPanel()
+					.getEPartnerConfig(i);
+			Object[] newEPartnerObject = { epartnerConfig.getEpartnerName(),
+					epartnerConfig.getEpartnerAmount() };
+			view.getPanel().getEntityPanel().getEPartnerTableModel()
+					.addRow(newEPartnerObject);
+		}
 	}
 }
