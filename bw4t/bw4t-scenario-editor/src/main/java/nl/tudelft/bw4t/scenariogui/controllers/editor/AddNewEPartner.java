1b6973c0f20b58e01794d170e6a129c327a99f82
Arun Malhoe <arunmalhoe@gmail.com>
2014-06-04 13:33:09 +0200

    fixed stuff that went wrong when merging

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/editor/AddNewEPartner.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/controllers/editor/AddNewEPartner.java
@@ -3,30 +3,26 @@ package nl.tudelft.bw4t.scenariogui.controllers.editor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import nl.tudelft.bw4t.scenariogui.EPartnerConfig;
 import nl.tudelft.bw4t.scenariogui.gui.panel.MainPanel;
 
 /**
  * Handles the event to create a new E-partner.
  * 
- * @version     0.1                
- * @since       12-05-2014        
+ * @version 0.1
+ * @since 12-05-2014
  */
 class AddNewEPartner implements ActionListener {
 
-    /**
-     * The <code>MainPanel</code> serving as the content pane.
-     */
     private MainPanel view;
-    
-    /**
-     * Keeps track of the amount of e-Partners created
-     */
+
     private int eCount;
-    
+
     /**
      * Create an AddNewEpartner event handler.
-     *
-     * @param newView The parent view.
+     * 
+     * @param newView
+     *            The parent view.
      */
     public AddNewEPartner(final MainPanel newView) {
         this.view = newView;
@@ -34,14 +30,18 @@ class AddNewEPartner implements ActionListener {
 
     /**
      * Executes action that needs to happen when the "New E-partner" button is
-     * pressed.
-     * Gives default name of "E-partner &lt;n&gt;" where &lt;n&gt; is the n'th e-Parnter created.
-     *   
-     * @param ae The action
+     * pressed. Gives default name of "E-partner &lt;n&gt;" where &lt;n&gt; is
+     * the n'th e-Parnter created.
+     * 
+     * @param ae
+     *            The action
      */
     public void actionPerformed(ActionEvent ae) {
-        eCount++;
-        Object[] newEPartnerObject = {"E-partner" + eCount, 1};
+        eCount = view.getEntityPanel().getEPartnerConfigs().size() + 1;
+        Object[] newEPartnerObject = { "E-partner " + eCount, 1 };
         view.getEntityPanel().getEPartnerTableModel().addRow(newEPartnerObject);
+        EPartnerConfig config = new EPartnerConfig();
+        config.setEpartnerName("E-Partner " + eCount);
+        view.getEntityPanel().getEPartnerConfigs().add(config);
     }
 }
