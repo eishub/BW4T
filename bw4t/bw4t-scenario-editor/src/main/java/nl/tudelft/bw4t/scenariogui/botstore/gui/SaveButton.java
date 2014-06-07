7c1093f8f022900e645cf7e1f2e31da5c1be67ce
Wendy <wendybolier@hotmail.com>
2014-06-08 01:02:53 +0200

    Fixed buttons: SaveButton and CancelButton

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/botstore/gui/SaveButton.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/botstore/gui/SaveButton.java
@@ -12,7 +12,7 @@ import nl.tudelft.bw4t.scenariogui.ScenarioEditor;
 import nl.tudelft.bw4t.scenariogui.editor.gui.MainPanel;
 
 /**
- * Handles actions of the applybutton
+ * Handles actions of the SaveButton
  */
 class SaveButton implements ActionListener {
 
@@ -29,8 +29,6 @@ class SaveButton implements ActionListener {
 	 */
 	public SaveButton(BotEditorPanel pview) {
 		this.view = pview;
-		BotEditor be = (BotEditor) SwingUtilities.getWindowAncestor(view);
-		mp = be.getParent();
 	}
 
 	/**
@@ -41,8 +39,8 @@ class SaveButton implements ActionListener {
 	 *            The action event caused by clicking on the button.
 	 */
 	public void actionPerformed(ActionEvent ae) {
-		String fileName = view.getFileNameField().getText();
-		String botName = view.getBotNameField().getText();
+		String fileName = view.getFileName();
+		String botName = view.getBotName();
 		String nonAlphaNumericRegex = "^[ a-zA-Z0-9_-]*$";
 		File f;
 		if (fileName.endsWith(".goal")) {
@@ -52,44 +50,24 @@ class SaveButton implements ActionListener {
 				if (name.matches(nonAlphaNumericRegex) || f.exists()) {
 					if (botName.length() > 0) {
 						if (botName.matches(nonAlphaNumericRegex)) {
-							view.getDataObject().setBotName(
-									view.getBotNameField().getText());
+							view.getDataObject().setBotName(view.getBotName());
 							view.getDataObject().setBotController(
 									EntityType.getType((String) view
 											.getBotControllerSelector()
 											.getSelectedItem()));
-							view.getDataObject()
-									.setBotAmount(
-											Integer.parseInt(view
-													.getBotAmountTextField()
-													.getText()));
-							view.getDataObject().setBotSize(
-									view.getSizeSlider().getValue());
-							view.getDataObject().setBotSpeed(
-									view.getSpeedSlider().getValue());
-							view.getDataObject().setBotBatteryCapacity(
-									view.getBatterySlider().getValue());
-							view.getDataObject()
-									.setGrippers(
-											view.getNumberOfGrippersSlider()
-													.getValue());
-							view.getDataObject().setBatteryEnabled(
-									view.getBatteryEnabledCheckbox()
-											.isSelected());
-							view.getDataObject().setColorBlindHandicap(
-									view.getColorblindCheckbox().isSelected());
-							view.getDataObject().setGripperHandicap(
-									view.getGripperCheckbox().isSelected());
-							view.getDataObject().setMoveSpeedHandicap(
-									view.getmovespeedCheckbox().isSelected());
-							view.getDataObject()
-									.setSizeOverloadHandicap(
-											view.getsizeoverloadCheckbox()
-													.isSelected());
-							view.getDataObject().setReferenceName(
-									view.getBotReferenceField().getText());
-							view.getDataObject().setFileName(
-									view.getFileNameField().getText());
+							view.getDataObject().setBotAmount(view
+													.getBotAmount());
+							view.getDataObject().setBotSize(view.getBotSize());
+							view.getDataObject().setBotSpeed(view.getBotSpeed());
+							view.getDataObject().setBotBatteryCapacity(view.getBotBatteryCapacity());
+							view.getDataObject().setGrippers(view.getGrippers());
+							view.getDataObject().setBatteryEnabled(view.isBatteryEnabled());
+							view.getDataObject().setColorBlindHandicap(view.getColorBlindHandicap());
+							view.getDataObject().setGripperHandicap(view.getGripperHandicap());
+							view.getDataObject().setMoveSpeedHandicap(view.getMoveSpeedHandicap());
+							view.getDataObject().setSizeOverloadHandicap(view.getSizeOverloadHandicap());
+							view.getDataObject().setReferenceName(view.getReferenceName());
+							view.getDataObject().setFileName(view.getFileName());
 
 							updateBotTable();
 
@@ -125,6 +103,9 @@ class SaveButton implements ActionListener {
 					"The file name is invalid.\n"
 							+ "File names should end in .goal.");
 		}
+		
+		view.getBotStoreController().updateConfig(view);
+		view.dispose();
 	}
 
 	/**
