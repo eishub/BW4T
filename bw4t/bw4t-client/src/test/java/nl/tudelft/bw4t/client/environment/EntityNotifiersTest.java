f63dc93da25f0e34d82bd5797a9a7a2046e92b3b
LiebenS <sander.liebens@home.nl>
2014-06-10 15:32:32 +0200

    notifyNewEntityTest added to Environment package in client
    Former-commit-id: a67143fa98a6ba1da4b0c115edf36084a6c939cd [formerly 198ca748945d534d1cfac67538efaf68eeb9a651]
    Former-commit-id: 4369ea76e64d129ef1ea9bc952e196977225c5b7

--- bw4t/bw4t-client/src/test/java/nl/tudelft/bw4t/client/environment/EntityNotifiersTest.java
+++ bw4t/bw4t-client/src/test/java/nl/tudelft/bw4t/client/environment/EntityNotifiersTest.java
@@ -1,15 +1,15 @@
 package nl.tudelft.bw4t.client.environment;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mockito;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
 import eis.EnvironmentListener;
 
 /**
@@ -19,16 +19,38 @@ import eis.EnvironmentListener;
 public class EntityNotifiersTest {
     
     private EnvironmentListener mockedEnvListener = Mockito.mock(eis.EnvironmentListener.class);
+    private EnvironmentListener mockedEnvListener2 = Mockito.mock(eis.EnvironmentListener.class);
+    private EnvironmentListener mockedEnvListener3 = Mockito.mock(eis.EnvironmentListener.class);
     private RemoteEnvironment mockedRemEnv = Mockito.mock(RemoteEnvironment.class);
     private Collection<String> mockedCollection = Mockito.mock(Collection.class);
     
-    @Test
-    public void notifyFreeEntityTest() {
+    @Before
+    public void createRemotes() {
         ArrayList<EnvironmentListener> listenerList = new ArrayList<EnvironmentListener>();
         listenerList.add(mockedEnvListener);
+        listenerList.add(mockedEnvListener2);
+        listenerList.add(mockedEnvListener3);
         when(mockedRemEnv.getEnvironmentListeners()).thenReturn(listenerList);
-        
+    }
+    
+    @Test
+    public void notifyFreeEntityTest() {
         EntityNotifiers.notifyFreeEntity("Bot1", mockedCollection, mockedRemEnv);
         Mockito.verify(mockedEnvListener).handleFreeEntity(any(String.class), any(Collection.class));
+        Mockito.verify(mockedEnvListener2).handleFreeEntity(any(String.class), any(Collection.class));
+        Mockito.verify(mockedEnvListener3).handleFreeEntity(any(String.class), any(Collection.class));
+    }
+    
+    @Test
+    public void notifyNewEntityTest() {        
+        EntityNotifiers.notifyNewEntity("Bot1", mockedRemEnv);
+        Mockito.verify(mockedEnvListener).handleNewEntity(any(String.class));
+        Mockito.verify(mockedEnvListener2).handleNewEntity(any(String.class));
+        Mockito.verify(mockedEnvListener3).handleNewEntity(any(String.class));
+    }
+    
+    @Test
+    public void notifyDeletedEntityTest() {
+        
     }
 }
