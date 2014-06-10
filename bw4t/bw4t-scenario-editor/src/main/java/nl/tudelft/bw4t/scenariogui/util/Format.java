26f906d758be725626fc9cf795a53ac79d451cab
Nick <gnick.com@gmail.com>
2014-06-10 09:14:37 +0200

    Fixed the memory out of bounds with the speed and size sliders.

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/util/Format.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/util/Format.java
@@ -104,4 +104,19 @@ public final class Format {
             amount *= -1;
         return (int) amount;
     }
+    
+    /**
+     * Pad the string with zeros.
+     * @param value The string to be padded.
+     * @param amount The amount of zeroes to add.
+     * @return The padded string.
+     */
+    public static String padString(String value, int amount) {
+        StringBuffer buf = new StringBuffer(); 
+        while (value.length() < amount) {
+            buf.append("0");
+        }
+        return buf.toString();
+    }
+    
 }
