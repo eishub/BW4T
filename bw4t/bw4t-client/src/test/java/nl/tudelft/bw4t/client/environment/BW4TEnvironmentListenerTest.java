1a88f2fdc98a909bc00a9698556f79d230074acc
Martin Rogalla <martin@martinrogalla.com>
2014-06-11 00:40:19 +0200

    Ignored not yet implemented tests.
    Former-commit-id: 88b6d19b42422db9dc793b534fe5b40280fd3d12 [formerly f43a9fe522e3d7d5b04b7c8d9a903bfe00848b95]
    Former-commit-id: a345f12a1c17052b476ed6b850959e5156bfd34c

--- bw4t/bw4t-client/src/test/java/nl/tudelft/bw4t/client/environment/BW4TEnvironmentListenerTest.java
+++ bw4t/bw4t-client/src/test/java/nl/tudelft/bw4t/client/environment/BW4TEnvironmentListenerTest.java
@@ -2,10 +2,12 @@ package nl.tudelft.bw4t.client.environment;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BW4TEnvironmentListenerTest {
 
+    @Ignore("Test is not yet implemented.")
     @Test
     public void test() {
         fail("Not yet implemented");
