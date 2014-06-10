e9217868cf188e28eed9693ef24e2ecc7c52e1bc
Tim van Rossum <trvanrossum@gmail.com>
2014-06-10 15:26:06 +0200

    Created a small test covering the method fully.
    Former-commit-id: e2ae25d6a7b3f40ab8a401363088c44badfa1f02 [formerly 29c174231330d0269e6916afa38a2f501b05702b]
    Former-commit-id: b56f7865ca9d7fbeae162521c5203e7867c5e01c

---
+++ bw4t/bw4t-map-editor/src/test/java/nl/tudelft/bw4t/map/editor/controller/RandomMapTest.java
@@ -0,0 +1,32 @@
+package nl.tudelft.bw4t.map.editor.controller;
+
+import org.junit.Test;
+
+public class RandomMapTest {
+	private Map m = new Map(10, 10, 10, true, true);
+	
+	@Test(expected = AssertionError.class)
+	public void illegalRowsTest() {
+		m.createRandomGrid(-1, 1, 6);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void illegalColTest() {
+		m.createRandomGrid(1, -1, 0);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void illegalAmountOfRoomsTest() {
+		m.createRandomGrid(1, 1, 100);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void illegalRowAndColTest() {
+		m.createRandomGrid(-1, -1, 20);
+	}
+	
+	@Test
+	public void legalMapTest() {
+		m.createRandomGrid(20, 20, 10);
+	}
+}
