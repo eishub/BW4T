dda7ca48bb082bddff547e0b69afdb92681c15f9
LiebenS <sander.liebens@home.nl>
2014-06-10 15:10:05 +0200

    First test in EntityNotifiers written en passed.

---
+++ bw4t/bw4t-client/src/test/java/nl/tudelft/bw4t/client/environment/BW4TEnvironmentListenerTest.java
@@ -0,0 +1,14 @@
+package nl.tudelft.bw4t.client.environment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BW4TEnvironmentListenerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ bw4t/bw4t-client/src/test/java/nl/tudelft/bw4t/client/environment/RemoteEnvironmentTest.java
@@ -0,0 +1,14 @@
+package nl.tudelft.bw4t.client.environment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RemoteEnvironmentTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
