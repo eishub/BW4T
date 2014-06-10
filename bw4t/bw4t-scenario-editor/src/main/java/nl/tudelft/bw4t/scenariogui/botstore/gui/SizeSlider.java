0693fcc2ce1a96913be85aeef5f0e6b163419018
Wendy <wendybolier@hotmail.com>
2014-06-10 11:20:19 +0200

    Moved all the calculations from SizeSlider to the controller and moved SizeSlider and SpeedSlider to the GUI package. Also deleted a lot of unnecessary/duplicated code.
    Former-commit-id: a718b0e3c9e8157950e7506bb396957271bac810 [formerly cb8a3fd21a29741f5e563349eda54c5aa4300b1a]
    Former-commit-id: cd577d962964baaa8b4f5b28a2e2e83ff38c6f32

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/botstore/gui/SizeSlider.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/botstore/gui/SizeSlider.java
@@ -1,14 +1,14 @@
-package nl.tudelft.bw4t.scenariogui.botstore.controller;
+package nl.tudelft.bw4t.scenariogui.botstore.gui;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import nl.tudelft.bw4t.scenariogui.botstore.gui.BotEditorPanel;
+import nl.tudelft.bw4t.scenariogui.botstore.controller.BotController;
 
 /**
- * Handles actions of the speedslider
+ * Handles actions of the sizeslider
  */
-class SpeedSlider extends MouseAdapter {
+class SizeSlider extends MouseAdapter {
     /**
      * The panel containing the slider.
      */
@@ -17,15 +17,11 @@ class SpeedSlider extends MouseAdapter {
      * Constructor.
      * @param pview The panel containing the slider.
      */
-    public SpeedSlider(BotEditorPanel pview) {
+    public SizeSlider(BotEditorPanel pview) {
         this.view = pview;
     }
     
-    /**
-     * Update the BatteryUseValueLabel with the correct value when the slider is moved.
-     * @param arg0 MouseEvent
-     */
-    @Override 
+    @Override
     public void mouseReleased(MouseEvent arg0) {
     	BotController currentController = view.getBotController();
     	currentController.setNewBatteryValue(view.getBotSpeed(), view.getBotSize(), view);
