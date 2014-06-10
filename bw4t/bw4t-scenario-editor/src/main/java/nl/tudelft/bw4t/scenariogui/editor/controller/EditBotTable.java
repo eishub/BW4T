66d0e9e5d4244008b61ac2819ff5fe408b355524
Joop Aué <JoopAue@gmail.com>
2014-06-10 10:56:29 +0200

    Fixed error in EditBotTable.java caused by merge

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/editor/controller/EditBotTable.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/editor/controller/EditBotTable.java
@@ -65,10 +65,10 @@ public class EditBotTable implements TableModelListener {
             config.setBotController(botController);
             break;
         case 2:
-            config.setFileName(value);
+            config.setFileName(newValue);
             break;
         case 3:
-            config.setBotAmount(Integer.parseInt(value));
+            config.setBotAmount(Integer.parseInt(newValue));
             break;
          default:
             break;
