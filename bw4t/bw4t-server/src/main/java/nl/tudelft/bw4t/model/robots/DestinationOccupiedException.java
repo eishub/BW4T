09a5343f4ded503facdcea861392b88be6a7c0d5
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-06-10 14:18:32 +0200

    Enable collisions between bots

---
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/model/robots/DestinationOccupiedException.java
@@ -0,0 +1,15 @@
+package nl.tudelft.bw4t.model.robots;
+
+/**
+ * Created by on 10-6-2014.
+ */
+public class DestinationOccupiedException extends Exception {
+
+    public DestinationOccupiedException() {
+
+    }
+
+    public DestinationOccupiedException(String message) {
+        super(message);
+    }
+}
