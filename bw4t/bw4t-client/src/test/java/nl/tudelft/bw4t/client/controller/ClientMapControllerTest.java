9bd1a09c948f626db03a07a971b6111a7e057899
Martin Rogalla <martin@martinrogalla.com>
2014-06-11 22:59:10 +0200

    Finished up the test for the ClientController, still working on the ClientMapControllerTest.
    Former-commit-id: ad9095816569b982db2d8abfef3d992746753125 [formerly 8559b7a92da5f115e3f71c3195a6226364e5d4e1]
    Former-commit-id: 18fb27b2b552e5b3d64795711561ff552cafbf23

--- bw4t/bw4t-client/src/test/java/nl/tudelft/bw4t/client/controller/ClientMapControllerTest.java
+++ bw4t/bw4t-client/src/test/java/nl/tudelft/bw4t/client/controller/ClientMapControllerTest.java
@@ -1,14 +1,26 @@
 package nl.tudelft.bw4t.client.controller;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+import eis.iilang.Function;
+import eis.iilang.Identifier;
+import eis.iilang.Numeral;
+import eis.iilang.Parameter;
+
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import nl.tudelft.bw4t.client.BW4TClient;
 import nl.tudelft.bw4t.client.environment.RemoteEnvironment;
+import nl.tudelft.bw4t.client.gui.BW4TClientGUI;
+import nl.tudelft.bw4t.client.startup.InitParam;
 import nl.tudelft.bw4t.map.NewMap;
 import nl.tudelft.bw4t.map.Point;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -23,6 +35,10 @@ public class ClientMapControllerTest {
     NewMap map;
     @Mock
     RemoteEnvironment remoteEnvironment;
+    @Mock
+    BW4TClientGUI clientGUI;
+    @Mock
+    BW4TClient client;
 
     private ClientMapController clientMapController;
     
@@ -40,21 +56,43 @@ public class ClientMapControllerTest {
     @Test
     public void testRun() {
         clientMapController.run();
+        verify(clientController, times(1)).updatedNextFrame();
+        verify(clientController, times(1)).getEnvironment();
     }
 
     @Test
     public void testUpdateRenderer() {
-        fail("Not yet implemented"); // TODO
+        clientMapController.updateRenderer(clientGUI);
+        verify(clientGUI, times(1)).validate();
+        verify(clientGUI, times(1)).repaint();
     }
 
     @Test
-    public void testClientMapController() {
-        fail("Not yet implemented"); // TODO
+    public void testHandlePerceptNotOccupied() {
+        LinkedList<Parameter> parameters = new LinkedList<Parameter>();
+        parameters.add(new Function("occupied", new Identifier("Room1")));
+        clientMapController.handlePercept("not", parameters);
+        
     }
-
+    
     @Test
-    public void testHandlePercept() {
-        fail("Not yet implemented"); // TODO
+    public void testHandlePerceptNotHolding() {
+        LinkedList<Parameter> parameters = new LinkedList<Parameter>();
+        parameters.add(new Function("holding", new Numeral(2)));
+        clientMapController.handlePercept("not", parameters);
+    }
+    
+    @Ignore("Working on this test case")
+    @Test
+    public void testHandlePerceptNull() throws RemoteException{
+        LinkedList<Parameter> parameters = new LinkedList<Parameter>();
+        parameters.add(new Function("occupied", new Identifier("Room1")));
+        when(clientController.getEnvironment()).thenReturn(remoteEnvironment);
+        HashMap<String, BW4TClientGUI> entityToGUI = new HashMap<String, BW4TClientGUI>();
+        entityToGUI.put(clientMapController.getTheBot().getName(), clientGUI);
+        when(remoteEnvironment.getEntityToGUI()).thenReturn(entityToGUI);
+        when(client.getAllPerceptsFromEntity(any(String.class))).thenReturn(null);
+        clientMapController.handlePercept("not", parameters);
     }
 
 }
