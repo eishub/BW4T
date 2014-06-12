2ad8f350ecca15067262b9f2e5999e315c90d6c9
Joost Rothweiler <joostrothweiler@gmail.com>
2014-06-12 11:01:50 +0200

    Update with every class now checkstyle proof.

--- bw4t/bw4t-map-editor/src/main/java/nl/tudelft/bw4t/map/editor/gui/ColorPaletteListener.java
+++ bw4t/bw4t-map-editor/src/main/java/nl/tudelft/bw4t/map/editor/gui/ColorPaletteListener.java
@@ -2,6 +2,10 @@ package nl.tudelft.bw4t.map.editor.gui;
 
 import nl.tudelft.bw4t.map.BlockColor;
 
+/**
+ * ColorPaletteListener for all the different colors on the palette.
+ *
+ */
 public interface ColorPaletteListener {
     void colorClicked(BlockColor c);
 }
