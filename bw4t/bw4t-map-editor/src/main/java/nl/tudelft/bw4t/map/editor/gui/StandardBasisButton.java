ec24fbcc91fa7faf2b6f459d6c87de1a1759600a
Wendy <wendybolier@hotmail.com>
2014-06-13 14:27:27 +0200

    Added SizeDialogController and made the actionlisteners. The BeginFromScratchButton finally works!!
    Former-commit-id: 25c77175aee0015a7d8d1cd9138f5dd45440a334 [formerly 10c1282aad6ba19297a5aa5ce26a3be6d5496cef]
    Former-commit-id: 0c49bbf636b43eab17ee4f1e26356c9d1edfd499

--- bw4t/bw4t-map-editor/src/main/java/nl/tudelft/bw4t/map/editor/gui/StandardBasisButton.java
+++ bw4t/bw4t-map-editor/src/main/java/nl/tudelft/bw4t/map/editor/gui/StandardBasisButton.java
@@ -1,5 +1,32 @@
 package nl.tudelft.bw4t.map.editor.gui;
 
-public class StandardBasisButton {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class StandardBasisButton implements ActionListener {
+
+	private SizeDialog view;
+
+	/**
+	 * The constructor for this action listener.
+	 * 
+	 * @param pview
+	 *            The frame with the button in it.
+	 */
+	
+	public StandardBasisButton(SizeDialog pview) {
+		this.view = pview;
+	}
+
+	/**
+	 * Perform the required action 
+	 * 
+	 * @param ae
+	 *            The action event triggering this method.
+	 */
+	public void actionPerformed(ActionEvent ae) {
+		// TODO fill in the right frame that has to be opened
+		SizeDialog dialog = new SizeDialog();
+    	dialog.setVisible(true);
+	}
 }
