395dd66cf5ee3cdd8cd2d8ebf5d4af0cb2ff90fe
LiebenS <sander.liebens@home.nl>
2014-06-13 19:18:57 +0200

    Added tests to NavigatingRobot so that AbstractRobot is being tested as well. Not finished yet.
    Former-commit-id: 0d60d34383b8977d7cea94035ba5d62eedeedab7 [formerly dae75f01830e047a6331c7d40166232ebbd3da7f]
    Former-commit-id: c7734d88b44e8854533aeeb52754a66bd9c1fe2e

--- bw4t/bw4t-server/src/test/java/nl/tudelft/bw4t/server/model/robots/NavigatingRobotTest.java
+++ bw4t/bw4t-server/src/test/java/nl/tudelft/bw4t/server/model/robots/NavigatingRobotTest.java
@@ -3,12 +3,18 @@ package nl.tudelft.bw4t.server.model.robots;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import nl.tudelft.bw4t.map.BlockColor;
+import nl.tudelft.bw4t.map.Zone;
 import nl.tudelft.bw4t.server.model.BoundedMoveableObject;
+import nl.tudelft.bw4t.server.model.blocks.Block;
 import nl.tudelft.bw4t.server.model.robots.NavigatingRobot.State;
 
 import org.junit.Before;
@@ -25,17 +31,32 @@ import repast.simphony.space.continuous.NdPoint;
 public class NavigatingRobotTest {
     
     private ContinuousSpace<Object> mockedSpace = Mockito.mock(ContinuousSpace.class);
+    private ContinuousSpace<Object> mockedOtherSpace = Mockito.mock(ContinuousSpace.class);
+    
     private Context<Object> mockedContext = Mockito.mock(Context.class);
+    private Context<Object> mockedOtherContext = Mockito.mock(Context.class);
+    
     private Queue<NdPoint> mockedQueue = Mockito.mock(ConcurrentLinkedQueue.class);
     private BoundedMoveableObject mockedMovableObject = Mockito.mock(BoundedMoveableObject.class);
+    //private BW4TEnvironment mockedEnv = Mockito.mock(BW4TEnvironment.class);
     
     private NavigatingRobot bot;
+    private NavigatingRobot bot2;
+    private NavigatingRobot bot3;
+    private NavigatingRobot bot4;
+    private NavigatingRobot bot5;
+    private NavigatingRobot bot6;
     
     @Before
     public void createNavigatingRobot() {
         int cap = 2;
-        String name = "Bot1";
-        bot = new NavigatingRobot(name, mockedSpace, mockedContext, true, cap);
+        int cap2 = 1;
+        bot = new NavigatingRobot("Bot1", mockedSpace, mockedContext, true, cap);
+        bot2 = new NavigatingRobot("Bot2", mockedSpace, mockedOtherContext, true, cap2);
+        bot3 = new NavigatingRobot("Bot3", mockedOtherSpace, mockedContext, true, cap2);
+        bot4 = null;
+        bot5 = new NavigatingRobot("Bot5", null, mockedContext, true, cap2);
+        bot6 = new NavigatingRobot("Bot5", null, mockedContext, true, cap2);
     }
 
     @Test
@@ -46,6 +67,10 @@ public class NavigatingRobotTest {
         
     }
     
+    /**
+     * TODO: Kijken naar gebruik mockedQueue, 
+     * is volgens mij niet nodig bij gebruik van Fieldreflection
+     */
     @Test
     public void useNextTargetNotEmptyTest() {
         when(mockedQueue.isEmpty()).thenReturn(false);
@@ -74,5 +99,47 @@ public class NavigatingRobotTest {
         assertEquals(bot.getState(), State.ARRIVED);
     }
 
+    @Test
+    public void getNameTest() {
+        assertEquals(bot.getName(), "Bot1");
+    }
+    
+    @Test
+    public void disconnectTest() {
+        bot.disconnect();
+        assertEquals(bot.isConnected(), false);
+    }
+    
+    @Test
+    public void equalseTest() {
+        Zone z = new Zone();
+        assertEquals(bot.equals(bot2), false);
+        assertEquals(bot.equals(bot3), false);
+        assertEquals(bot5.equals(bot), false);
+        assertEquals(bot.equals(bot4), false);
+        assertEquals(bot.equals(z), false);
+        
+        assertEquals(bot.equals(bot), true);
+        assertEquals(bot5.equals(bot6), true);
+    }
+    
+    @Test
+    public void hashCodeTest() {
+        assertEquals(bot.hashCode(), 1);
+    }
+    
+    @Test
+    public void isHoldingTest() {
+        List<Block> emptyList = new ArrayList<Block>();;
+        assertEquals(bot.isHolding().isEmpty(), emptyList.isEmpty());
+    }
+    
+    @Test
+    public void canPickUpTest() {
+        assertEquals(bot.canPickUp(bot2), false);
+        Block b = new Block(BlockColor.BLUE, mockedOtherSpace, mockedContext);
+        assertEquals(bot.canPickUp(b), true);
+    }
+    
 
 }
