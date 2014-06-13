21b6e17431922bd692d310b6e52bf8b542fc9370
Tim van Rossum <trvanrossum@gmail.com>
2014-06-13 12:47:51 +0200

    Refactored The MapPanelController as it was getting way too big. The randomizer is in its own utility class now.

--- bw4t/bw4t-map-editor/src/test/java/nl/tudelft/bw4t/map/editor/controller/RandomMapTest.java
+++ bw4t/bw4t-map-editor/src/test/java/nl/tudelft/bw4t/map/editor/controller/RandomMapTest.java
@@ -3,30 +3,28 @@ package nl.tudelft.bw4t.map.editor.controller;
 import org.junit.Test;
 
 public class RandomMapTest {
-	private MapPanelController m = new MapPanelController(10, 10, 10, true, true);
-	
 	@Test(expected = AssertionError.class)
 	public void illegalRowsTest() {
-		m.createRandomGrid(-1, 1, 6);
+		RandomMapCreator.createRandomGrid(-1, 1, 6);
 	}
 	
 	@Test(expected = AssertionError.class)
 	public void illegalColTest() {
-		m.createRandomGrid(1, -1, 0);
+		RandomMapCreator.createRandomGrid(1, -1, 0);
 	}
 	
 	@Test(expected = AssertionError.class)
 	public void illegalAmountOfRoomsTest() {
-		m.createRandomGrid(1, 1, 100);
+		RandomMapCreator.createRandomGrid(1, 1, 100);
 	}
 	
 	@Test(expected = AssertionError.class)
 	public void illegalRowAndColTest() {
-		m.createRandomGrid(-1, -1, 20);
+		RandomMapCreator.createRandomGrid(-1, -1, 20);
 	}
 	
 	@Test
 	public void legalMapTest() {
-		m.createRandomGrid(20, 20, 10);
+		RandomMapCreator.createRandomGrid(20, 20, 10);
 	}
 }
