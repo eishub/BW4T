66f19ecbf82519638dbd9fa22fcb38f0c1e19223
Seu Man To <smto@Shadower.thehive.dnsalias.org>
2014-06-16 11:54:19 +0200

    Dropdown contains only bots (and empty spaces)

--- bw4t/bw4t-client/src/main/java/nl/tudelft/bw4t/client/gui/menu/ComboAgentModel.java
+++ bw4t/bw4t-client/src/main/java/nl/tudelft/bw4t/client/gui/menu/ComboAgentModel.java
@@ -1,22 +1,56 @@
 package nl.tudelft.bw4t.client.gui.menu;
 
+import java.util.Collection;
+
 import javax.swing.AbstractListModel;
 import javax.swing.ComboBoxModel;
 
+import eis.exceptions.EntityException;
 import nl.tudelft.bw4t.client.gui.BW4TClientGUI;
+import nl.tudelft.bw4t.network.BW4TServerHiddenActions;
+import nl.tudelft.bw4t.network.BW4TServerActions;
+import nl.tudelft.bw4t.server.BW4TServer;
 
 public class ComboAgentModel extends AbstractListModel implements ComboBoxModel {
 
     private final BW4TClientGUI gui;
+    //private final BW4TServer server;
     private String selection = "";
 
-    public ComboAgentModel(BW4TClientGUI clientGUI) {
+    public ComboAgentModel(BW4TClientGUI clientGUI/*, BW4TServer server*/) {
         this.gui = clientGUI;
+        //this.server = server;
     }
 
     @Override
     public Object getElementAt(int arg0) {
-        return gui.environment.getAgents().get(arg0);
+   /*     List<String> bots = new ArrayList<String>();
+        Collection<String> entities = server.getEntities();
+        for(Iterator<String> iterator = entities.iterator(); iterator.hasNext();) {
+            String entity = iterator.next();
+            
+            if(getType(entity).toString().equals("Epartner")) {
+                continue;
+            } else {
+                bots = bots.add(entity);
+            }
+        }
+        
+        return bots.get(arg0);*/
+        try {
+            if(gui.environment.getType(gui.environment.getAgents().get(arg0)).equals("epartner")) {
+                return null;
+            } else {
+                return gui.environment.getAgents().get(arg0);
+            }
+        } catch (EntityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return null;
+        
+        //return gui.environment.getAgents().get(arg0);
     }
 
     @Override
