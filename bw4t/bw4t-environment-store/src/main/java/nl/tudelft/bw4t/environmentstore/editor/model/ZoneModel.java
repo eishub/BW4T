76cee039a77680350ff8c8dc7174e2fa3585a0d2
Valentine Mairet <val.mairet@gmail.com>
2014-06-16 10:02:42 +0200

    Deleted class that was actually useless
    ZoneData
    - used a ZoneModel now
    
    
    Former-commit-id: a0266dda175f9cb1112dfdc3f1c27d8f85432c97 [formerly 13ba6164caf99447509531f593dd664ee265d97f]
    Former-commit-id: 0c7ac0b9c54ee2c99d7a724c3d5bc4b13fb46c08

--- bw4t/bw4t-environment-store/src/main/java/nl/tudelft/bw4t/environmentstore/editor/model/ZoneModel.java
+++ bw4t/bw4t-environment-store/src/main/java/nl/tudelft/bw4t/environmentstore/editor/model/ZoneModel.java
@@ -3,8 +3,10 @@ package nl.tudelft.bw4t.environmentstore.editor.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import nl.tudelft.bw4t.environmentstore.editor.controller.MapPanelController;
 import nl.tudelft.bw4t.environmentstore.editor.model.Node.DoorDirection;
 import nl.tudelft.bw4t.map.BlockColor;
+import nl.tudelft.bw4t.map.Zone;
 import nl.tudelft.bw4t.map.Zone.Type;
 
 /**
@@ -29,8 +31,22 @@ public class ZoneModel {
 	
 	private String coordiname = "";
 	
+	private int row;
+	private int column;
+	
 	public ZoneModel() {
-		
+	}
+	
+	public ZoneModel(Zone zone) {
+		this.type = zone.getType();
+
+		this.row = getRow(zone);
+		this.column = getColumn(zone);
+
+		this.colors = zone.getBlocks();
+
+		this.isStartZone = isStartZone(zone);
+		this.isDropZone = isDropZone(zone);
 	}
 	
 	public ZoneModel(Node n) {
@@ -127,4 +143,47 @@ public class ZoneModel {
 	public void setStartZone(boolean isSZ) {
 		isStartZone = isSZ;
 	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+	/**
+	 * @param zone
+	 *            The current zone.
+	 * @return
+	 *        The row the zone belongs to. 
+	 */
+	private int getRow(Zone zone) {
+		double height = MapPanelController.ROOMHEIGHT;
+		double y = zone.getBoundingbox().getY();
+
+		return (int) ((y - height / 2) / height);
+	}
+	
+	/**
+	 * @param zone
+	 *            The current zone.
+	 * @return
+	 *        The column the zone belongs to. 
+	 */
+	private int getColumn(Zone zone) {
+		double width = MapPanelController.ROOMWIDTH;
+		double x = zone.getBoundingbox().getX();
+
+		return (int) ((x - width / 2) / width);
+	}
+
+	private boolean isStartZone(Zone zone) {
+		return zone.getName().startsWith("StartZone");
+	}
+
+	private boolean isDropZone(Zone zone) {
+		return zone.getName().startsWith("DropZone");
+	}
 }
+
