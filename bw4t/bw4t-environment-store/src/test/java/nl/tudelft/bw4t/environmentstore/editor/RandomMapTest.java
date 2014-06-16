36ad0f08762e60915b1c94e735ae38a62ff44740
Valentine Mairet <val.mairet@gmail.com>
2014-06-16 10:55:07 +0200

    restored the tests i lost :(
    Former-commit-id: 5021bc1b8a4c8498af0626075f9c468f947e6422 [formerly 9d2385a8f89c9f3018d85ea755c1f0f5cd3b596a]
    Former-commit-id: 7adf27fb864768edf4253a75bf82e5851e1bd34b

---
+++ bw4t/bw4t-environment-store/src/test/java/nl/tudelft/bw4t/environmentstore/editor/RandomMapTest.java
@@ -0,0 +1,32 @@
+package nl.tudelft.bw4t.environmentstore.editor;
+
+import nl.tudelft.bw4t.environmentstore.editor.model.RandomMapCreator;
+
+import org.junit.Test;
+
+public class RandomMapTest {
+	@Test(expected = AssertionError.class)
+	public void illegalRowsTest() {
+		RandomMapCreator.createRandomGrid(-1, 1, 6);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void illegalColTest() {
+		RandomMapCreator.createRandomGrid(1, -1, 0);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void illegalAmountOfRoomsTest() {
+		RandomMapCreator.createRandomGrid(1, 1, 100);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void illegalRowAndColTest() {
+		RandomMapCreator.createRandomGrid(-1, -1, 20);
+	}
+	
+	@Test
+	public void legalMapTest() {
+		RandomMapCreator.createRandomGrid(20, 20, 10);
+	}
+}
