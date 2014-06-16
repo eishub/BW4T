ea3d45ad3939a7480ec2654551487c78d301c92e
Valentine Mairet <val.mairet@gmail.com>
2014-06-16 12:29:43 +0200

    new sizeDialog
    Former-commit-id: b5cf485bfaf87ff61c09f605db298242fd2ed8fa [formerly 2e0c59887641430db089f0a3a72b4b3b51f193e9]
    Former-commit-id: 901357c9bd5ba2a8aa7eb9baf4fef34d77bc3b27

--- bw4t/bw4t-environment-store/src/main/java/nl/tudelft/bw4t/environmentstore/sizedialog/view/SizeDialog.java
+++ bw4t/bw4t-environment-store/src/main/java/nl/tudelft/bw4t/environmentstore/sizedialog/view/SizeDialog.java
@@ -1,88 +1,57 @@
 package nl.tudelft.bw4t.environmentstore.sizedialog.view;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import net.miginfocom.swing.MigLayout;
-import nl.tudelft.bw4t.environmentstore.editor.controller.MapPanelController;
-import nl.tudelft.bw4t.environmentstore.sizedialog.controller.SizeDialogController;
-
-import javax.swing.JLayeredPane;
-import javax.swing.JSplitPane;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JTextField;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JComboBox;
-import javax.swing.JCheckBox;
+import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
 
+import nl.tudelft.bw4t.environmentstore.sizedialog.controller.SizeDialogController;
+
 /**
- * The SizeDialog shows the user a dialog where he can enter the size of the map.
+ * The SizeDialog shows the user a dialog where he can enter the size of the
+ * map.
  */
 public class SizeDialog extends JFrame {
-	
+
+	private static final long serialVersionUID = -3691483561210215655L;
+
 	/**
 	 * The contentPane.
 	 */
 	private JPanel contentPane = new JPanel();
-	
+
 	/**
-	 * The left panel of the frame (which will hold the editExistingMapButton)
+	 * With this button the user can choose to start building a map from
+	 * scratch.
 	 */
-	private JPanel leftPanel = new JPanel();
-	
-	/**
-	 * The right panel of the frame (which will hold the rest)
-	 */
-	private JPanel rightPanel = new JPanel();
-	
-	/**
-	 * With this button the user can choose to start building a map from scratch.
-	 */
-	private JButton beginFromScratchButton = new JButton("Begin from scratch");
-	
-	/**
-	 * With this button the user can choose to use a standard basis.
-	 */
-	private JButton useStandardBasisButton = new JButton("Use standard basis");
-	
+	private JButton startButton = new JButton("Start");
+
 	/**
 	 * Label with the text #rows.
 	 */
 	private JLabel rowsLabel = new JLabel("#rows");
-	
+
 	/**
 	 * Label with the text #columns.
 	 */
 	private JLabel columnsLabel = new JLabel("#columns");
-	
-	/**
-	 * Label with the text {@link #entities}.
-	 */
-	private JLabel entityLabel = new JLabel("#entities");
-	
+
 	/**
 	 * Here can the user set the wanted numbers of rows.
 	 */
 	private JSpinner rows = new JSpinner();
-	
+
 	/**
 	 * Here can the user set the wanted number of columns.
 	 */
 	private JSpinner cols = new JSpinner();
-	
-	/**
-	 * Here can the user set the wanted number of entities.
-	 */
-	private JSpinner entities = new JSpinner();
-	
+
 	/**
 	 * SizeDialogController
 	 */
@@ -92,97 +61,84 @@ public class SizeDialog extends JFrame {
 	 * Create the frame.
 	 */
 	public SizeDialog() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		contentPane.setLayout(new GridBagLayout());
+		setContentPane(contentPane);
+
 		sdc = new SizeDialogController(this);
-		
-		setTitle("Size Dialog");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 230);
-		getContentPane().setLayout(new MigLayout("", "[grow][grow][][][][][][][][grow]", "[][][][grow]"));
-		
-		getContentPane().add(leftPanel, "cell 0 0 1 4,alignx left,growy");
-		leftPanel.setLayout(new MigLayout("", "[113px]", "[23px]"));
-		
-		getContentPane().add(rightPanel, "cell 1 1 9 3,grow");
-		rightPanel.setLayout(new MigLayout("", "[grow][grow]", "[][][][][][][]"));
-	
-		
+
 		SpinnerModel rowmodel = new SpinnerNumberModel(5, // initial value
-	            5, // min
-	            24, // max
-	            1); // step
-	    rows = new JSpinner(rowmodel);
-	    rightPanel.add(rows, "cell 1 0");
-
-	    SpinnerModel colmodel = new SpinnerNumberModel(5, // initial value
-	            3, // min
-	            24, // max
-	            1); // step
-	    cols = new JSpinner(colmodel);
-	    rightPanel.add(cols, "cell 1 1");
-	    
-	    SpinnerModel entitymodel = new SpinnerNumberModel(1, // initial value
-	            1, // min
-	            Integer.MAX_VALUE, // max
-	            1); // step
-	    entities = new JSpinner(entitymodel);
-	    rightPanel.add(entities, "cell 1 2");
-	    
-	    rightPanel.add(rowsLabel, "cell 0 0,alignx trailing");
-		rightPanel.add(columnsLabel, "cell 0 1,alignx trailing");
-	    rightPanel.add(entityLabel, "cell 0 2,alignx right");
-		rightPanel.add(beginFromScratchButton, "cell 0 6");
-		rightPanel.add(useStandardBasisButton, "cell 1 6");
+				5, // min
+				24, // max
+				1); // step
+		rows = new JSpinner(rowmodel);
 		
-		setLocationRelativeTo(null);
-			
-	}
-	
-    /**
-     * get {@link #rows} as set by user
-     * 
-     * @return {@link #rows}
-     */
-    public int getRows() {
-        return (Integer) (rows.getValue());
-    }
+		contentPane.add(rowsLabel, setUpContraints(GridBagConstraints.HORIZONTAL, 0, 0, 0));
+		contentPane.add(rows, setUpContraints(GridBagConstraints.HORIZONTAL, 1, 0, 0));
 
-    /**
-     * get {@link #cols} as set by user
-     * 
-     * @return {@link #cols}
-     */
-    public int getColumns() {
-        return (Integer) (cols.getValue());
-    }
-    
-    /**
-     * get {@link #entities} as set by user
-     * 
-     * @return {@link #entities}
-     */
-    public int getEntities() {
-    	return (Integer) (entities.getValue());
-    }
-	
-    /**
-     * @return beginFromScratchButton
-     */
-	public JButton getBeginFromScratchButton() {
-		return beginFromScratchButton;
+		SpinnerModel colmodel = new SpinnerNumberModel(5, // initial value
+				3, // min
+				24, // max
+				1); // step
+		cols = new JSpinner(colmodel);
+		contentPane.add(columnsLabel, setUpContraints(GridBagConstraints.HORIZONTAL, 0, 1, 0));
+		contentPane.add(cols, setUpContraints(GridBagConstraints.HORIZONTAL, 1, 1, 0));
+		
+		contentPane.add(startButton, setUpContraints(GridBagConstraints.BASELINE, 0, 2, 2));
+
+		setLocationRelativeTo(null);
+		pack();
+		setVisible(true);
+		setResizable(false);
+
 	}
 	
+	private GridBagConstraints setUpContraints(int constraints, int x, int y, int width) {
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.fill = constraints;
+		c.gridx = x;
+		c.gridy = y;
+		
+		if (width != 0) {
+			c.gridwidth = width;
+		}
+		
+		return c;
+	}
+
 	/**
-     * @return useStandardBasisButton
-     */
-	public JButton getStandardBasisButton() {
-		return useStandardBasisButton;
+	 * get {@link #rows} as set by user
+	 * 
+	 * @return {@link #rows}
+	 */
+	public int getRows() {
+		return (Integer) (rows.getValue());
 	}
-	
+
+	/**
+	 * get {@link #cols} as set by user
+	 * 
+	 * @return {@link #cols}
+	 */
+	public int getColumns() {
+		return (Integer) (cols.getValue());
+	}
+
+	/**
+	 * @return beginFromScratchButton
+	 */
+	public JButton getStartButton() {
+		return startButton;
+	}
+
 	/**
 	 * Return the SizeDialogController
+	 * 
 	 * @return sdc
 	 */
-	public SizeDialogController getSizeDialogController() { 
+	public SizeDialogController getSizeDialogController() {
 		return sdc;
-	}   
+	}
 }
