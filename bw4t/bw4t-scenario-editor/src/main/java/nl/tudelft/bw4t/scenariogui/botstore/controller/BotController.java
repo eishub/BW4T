08270b21ec24a99962838b771e47ececf7bc1993
Wendy <wendybolier@hotmail.com>
2014-06-16 11:19:24 +0200

    Fixed 5 failing tests and changed max number of entities.
    Former-commit-id: 848b5a309a7a0e8516a37d0e6eee7ce040374bb4 [formerly 51b538cf50ae288f63ad40543d05786f62630693]
    Former-commit-id: 63a0083263f820a48b53b4736a5b5f7c8617653f

--- bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/botstore/controller/BotController.java
+++ bw4t/bw4t-scenario-editor/src/main/java/nl/tudelft/bw4t/scenariogui/botstore/controller/BotController.java
@@ -10,7 +10,6 @@ import javax.swing.JFileChooser;
 import nl.tudelft.bw4t.map.EntityType;
 import nl.tudelft.bw4t.scenariogui.BW4TClientConfig;
 import nl.tudelft.bw4t.scenariogui.BotConfig;
-import nl.tudelft.bw4t.scenariogui.botstore.gui.BotEditor;
 import nl.tudelft.bw4t.scenariogui.botstore.gui.BotEditorPanel;
 import nl.tudelft.bw4t.scenariogui.botstore.gui.BotStoreViewInterface;
 import nl.tudelft.bw4t.scenariogui.editor.gui.MainPanel;
