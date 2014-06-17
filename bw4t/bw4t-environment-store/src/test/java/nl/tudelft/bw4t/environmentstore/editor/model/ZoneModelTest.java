f86978ced93585fff883a2e54cf9958fed3de107
Tim van Rossum <trvanrossum@gmail.com>
2014-06-17 16:08:47 +0200

    Extended the zonemodel test class.
    Former-commit-id: ff906e8f8f9850c8837663be6c356b7ae58c3cdb [formerly 7138014187c35b6e9c18be25f1b2bd42568fd907]
    Former-commit-id: 9a73a88f7f1fc26cdb053d22f371869c69fdde6f

--- bw4t/bw4t-environment-store/src/test/java/nl/tudelft/bw4t/environmentstore/editor/model/ZoneModelTest.java
+++ bw4t/bw4t-environment-store/src/test/java/nl/tudelft/bw4t/environmentstore/editor/model/ZoneModelTest.java
@@ -1,7 +1,15 @@
 package nl.tudelft.bw4t.environmentstore.editor.model;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.tudelft.bw4t.environmentstore.editor.model.Node.DoorDirection;
+import nl.tudelft.bw4t.map.BlockColor;
+import nl.tudelft.bw4t.map.Rectangle;
 import nl.tudelft.bw4t.map.Zone;
+import nl.tudelft.bw4t.map.Zone.Type;
 
 import org.junit.Test;
 
@@ -42,5 +50,110 @@ public class ZoneModelTest {
         zone = new ZoneModel();
         zone.generateNameFromPosition(100, 702);
         assertEquals(Zone.CORRIDOR_NAME + "AAA101", zone.getName());
+    }
+    
+    @Test
+    public void constructorTest() {
+    	Zone z = new Zone();
+    	ZoneModel zm = new ZoneModel(z);
+    	assertEquals(zm.getType(), Type.ROOM);
+    }
+    
+    @Test
+    public void constructorTestNode() {
+    	Node n = new Node(Type.ROOM);
+    	n.setDir(DoorDirection.NORTH);
+    	ZoneModel zm = new ZoneModel(n);
+    	assertEquals(zm.getType(), Type.ROOM);
+    	assertTrue(zm.hasDoor(0));
+    	n.setDir(DoorDirection.EAST);
+    	zm = new ZoneModel(n);
+    	assertTrue(zm.hasDoor(1));
+    	n.setDir(DoorDirection.SOUTH);
+    	zm = new ZoneModel(n);
+    	assertTrue(zm.hasDoor(2));
+    	n.setDir(DoorDirection.WEST);
+    	zm = new ZoneModel(n);
+    	assertTrue(zm.hasDoor(3));
+    	n.setType(Type.CORRIDOR);
+    	zm = new ZoneModel(n);
+    	assertFalse(zm.hasDoor(0));
+    	assertFalse(zm.hasDoor(1));
+    	assertFalse(zm.hasDoor(2));
+    	assertFalse(zm.hasDoor(3));
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void setColorNullTest() {
+    	ZoneModel zm = new ZoneModel();
+    	zm.setColors(null);
+    }
+    
+    @Test
+    public void setColorTest() {
+    	List<BlockColor> l = new ArrayList<BlockColor>();
+    	ZoneModel zm = new ZoneModel();
+    	zm.setColors(l);
+    	assertEquals(zm.getColors(), l);
+    }
+    
+    @Test
+    public void BlockColorsRoomTest() {
+    	List<BlockColor> l = new ArrayList<BlockColor>();
+    	l.add(BlockColor.BLUE);
+    	l.add(BlockColor.BLUE);
+    	ZoneModel zm = new ZoneModel();
+    	zm.setType(Type.ROOM);
+    	zm.setColors(l);
+    	assertEquals(zm.getColors(), l);
+    }
+    
+    @Test
+    public void BlockColorsCorridorTest() {
+    	List<BlockColor> l = new ArrayList<BlockColor>();
+    	l.add(BlockColor.BLUE);
+    	l.add(BlockColor.BLUE);
+    	ZoneModel zm = new ZoneModel();
+    	zm.setType(Type.CORRIDOR);
+    	zm.setColors(l);
+    	assertNotEquals(zm.getColors(), l);
+    }
+    
+    @Test
+    public void spawnCountCorridorTest() {
+    	ZoneModel zm = new ZoneModel();
+    	zm.setType(Type.CORRIDOR);
+    	assertEquals(zm.getSpawnCount(), 0);
+    }
+    
+    @Test
+    public void spawnCountStartZoneTest() {
+    	ZoneModel zm = new ZoneModel();
+    	zm.setType(Type.CORRIDOR);
+    	zm.setStartZone(true);
+    	assertEquals(zm.getSpawnCount(), 4);
+    }
+    
+    @Test
+    public void getZoneTest() {
+    	Zone z = new Zone();
+    	ZoneModel zm = new ZoneModel(z);
+    	assertEquals(zm.getZone(), z);
+    }
+    
+    @Test
+    public void calcColTest() {
+    	Rectangle r = new Rectangle(100.0, 100.0, 100.0, 100.0);
+    	Zone z = new Zone();
+    	z.setBoundingbox(r);
+    	assertTrue(ZoneModel.calcColumn(z) == 9);
+    }
+    
+    @Test
+    public void calcRowTest() {
+    	Rectangle r = new Rectangle(100.0, 100.0, 100.0, 100.0);
+    	Zone z = new Zone();
+    	z.setBoundingbox(r);
+    	assertTrue(ZoneModel.calcRow(z) == 9);
     }
 }
