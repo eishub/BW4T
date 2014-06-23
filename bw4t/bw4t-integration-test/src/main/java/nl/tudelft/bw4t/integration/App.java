f44167002cbf5549001014d8cf756c05bcc9ebfd
Jan Giesenberg <giesenja@gmail.com>
2014-06-23 12:12:19 +0200

    Remove unecessary 'Mock' objects and hopefully fix findbugs
    Former-commit-id: 14826d602b4b0fe7ff439a6d60192ecf958704e1 [formerly 13bd02c87d99c3ec399aa11a73dc26b8efbb2c83]
    Former-commit-id: d83f77df11686ba7999d267a04d37b560d8d9b1c

---
+++ bw4t/bw4t-integration-test/src/main/java/nl/tudelft/bw4t/integration/App.java
@@ -0,0 +1,10 @@
+package nl.tudelft.bw4t.integration;
+
+/**
+ * This is a placeholder so the src/main/java does not get deleted
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
