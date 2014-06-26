b6478c83922bc5ba87e19221df933a28fe126d37
Jan Giesenberg <giesenja@gmail.com>
2014-06-26 16:22:38 +0200

    changed the epartner percept to include the functionalities of the epartner

--- bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/eis/translators/EPartnerTranslator.java
+++ bw4t/bw4t-server/src/main/java/nl/tudelft/bw4t/server/eis/translators/EPartnerTranslator.java
@@ -1,13 +1,16 @@
 package nl.tudelft.bw4t.server.eis.translators;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import nl.tudelft.bw4t.server.model.epartners.EPartner;
+import nl.tudelft.bw4t.server.model.robots.handicap.IRobot;
 import eis.eis2java.exception.TranslationException;
 import eis.eis2java.translation.Java2Parameter;
 import eis.iilang.Identifier;
 import eis.iilang.Numeral;
 import eis.iilang.Parameter;
-
-import nl.tudelft.bw4t.server.model.epartners.EPartner;
-import nl.tudelft.bw4t.server.model.robots.handicap.IRobot;
+import eis.iilang.ParameterList;
 
 /**
  * Translates {@link EPartner} objects into parameters that can be used in eis.
@@ -16,7 +19,7 @@ public class EPartnerTranslator implements Java2Parameter<EPartner> {
 
     @Override
     public Parameter[] translate(EPartner o) throws TranslationException {
-        Parameter[] params = new Parameter[3];
+        Parameter[] params = new Parameter[4];
         params[0] = new Numeral(o.getId());
         params[1] = new Identifier(o.getName());
         final IRobot holder = o.getHolder();
@@ -25,6 +28,11 @@ public class EPartnerTranslator implements Java2Parameter<EPartner> {
         } else {
             params[2] = new Numeral(holder.getId());
         }
+        Collection<Parameter> types = new ArrayList<Parameter>();
+        for (String type : o.getTypeList()) {
+            types.add(new Identifier(type));
+        }
+        params[3] = new ParameterList(types);
         return params;
     }
 
