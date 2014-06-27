a60bf21fca432ffeec548dc16188d06bf97456c9
Tom Peeters <tompeeters368@gmail.com>
2014-06-27 13:20:44 +0200

    Updated Integration tests to new speed
    Enabled the GUI on integration tests.
    Made the integration tests properly all run, instead of only BlockTest,
    now that the server can disconnect bots.

--- bw4t/bw4t-integration-test/src/test/java/nl/tudelft/bw4t/client/BlockTest.java
+++ bw4t/bw4t-integration-test/src/test/java/nl/tudelft/bw4t/client/BlockTest.java
@@ -1,26 +1,25 @@
 package nl.tudelft.bw4t.client;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import eis.eis2java.exception.TranslationException;
 import eis.eis2java.translation.Translator;
+import eis.exceptions.ActException;
 import eis.exceptions.ManagementException;
 import eis.iilang.Action;
 import eis.iilang.Parameter;
 import eis.iilang.Percept;
-
 import java.io.IOException;
 import java.util.Iterator;
-
 import javax.xml.bind.JAXBException;
-
 import nl.tudelft.bw4t.client.environment.Launcher;
 import nl.tudelft.bw4t.client.environment.RemoteEnvironment;
 import nl.tudelft.bw4t.map.Zone;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import repast.simphony.scenario.ScenarioLoadException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * We test if blocks are properly perceived, picked up and delivered.
@@ -31,6 +30,7 @@ public class BlockTest {
      * Client to be used for testing.
      */
     private RemoteEnvironment client;
+    private String bot;
 
     /**
      * Launch the client and set it for use by the TestFunctions class.
@@ -50,20 +50,30 @@ public class BlockTest {
 
         client = Launcher.launch(clientArgs);
         TestFunctions.setClient(client);
+        bot = client.getAgents().get(0);
+    }
+
+    /** 
+     * Disconnects the bots from the server.
+     * @throws ManagementException Thrown if the client cannot disconnect a bot.
+     */
+    @After
+    public void tearDown() throws ManagementException {
+        client.killHumanEntity(bot);
     }
 
     /**
      * Here we test the picking up and delivering of a block.
-     * @throws Exception if the test fails
+     * @throws TranslationException If the test fails.
+     * @throws ActException If the test fails.
+     * @throws InterruptedException May be thrown while sleeping.
      */
     @Test
-    public void blockTest() throws Exception {
-        String bot = client.getAssociatedEntities(client.getAgents().get(0)).iterator().next();
-        
+    public void blockTest() throws TranslationException, ActException, InterruptedException {
         // Move to RoomC1
         Parameter[] param = Translator.getInstance().translate2Parameter("RoomC1");
         client.performAction(bot, new Action("goTo", param));
-        Thread.sleep(2000L);
+        Thread.sleep(4000L);
         
         // Check if the yellow block is present and move to it
         // It should always be present on the Banana map
@@ -73,26 +83,26 @@ public class BlockTest {
         while (percepts.hasNext()) {
             Percept percept = percepts.next();
             if (percept.toProlog().startsWith("color(") && percept.toProlog().endsWith(",YELLOW)")) {
-                blockNumber = Integer.parseInt(percept.toProlog().replace("color(","").replace(",YELLOW)",""));
+                blockNumber = Integer.parseInt(percept.toProlog().replace("color(", "").replace(",YELLOW)", ""));
             }
         }
         assertTrue(blockNumber != -1);
         param = Translator.getInstance().translate2Parameter(blockNumber);
         client.performAction(bot, new Action("goToBlock", param));
-        Thread.sleep(500L);
+        Thread.sleep(1000L);
         
         // Pick the block up
         client.performAction(bot, new Action("pickUp"));
-        Thread.sleep(200L);
+        Thread.sleep(400L);
         
         // Move to the DropZone
         param = Translator.getInstance().translate2Parameter(Zone.DROP_ZONE_NAME);
         client.performAction(bot, new Action("goTo", param));
-        Thread.sleep(3000L);
+        Thread.sleep(6000L);
         
         // Put the block down and verify the sequence index increased
         client.performAction(bot, new Action("putDown"));
-        Thread.sleep(200L);
+        Thread.sleep(400L);
         TestFunctions.retrievePercepts(bot);
         assertEquals(1, client.getEntityController(bot).getMapController().getSequenceIndex());
     }
