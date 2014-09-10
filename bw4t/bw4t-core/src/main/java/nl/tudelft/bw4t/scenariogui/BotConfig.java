8000a532d968ad7cf8f03c921bd4d35c5e971127
wouter <W.Pasman@TUDelft.nl>
2014-09-10 09:49:39 +0200

    #3198 bot scenario editor now has max 100% speed

--- bw4t/bw4t-core/src/main/java/nl/tudelft/bw4t/scenariogui/BotConfig.java
+++ bw4t/bw4t-core/src/main/java/nl/tudelft/bw4t/scenariogui/BotConfig.java
@@ -14,205 +14,208 @@ import nl.tudelft.bw4t.map.EntityType;
  */
 
 public final class BotConfig implements Serializable, Cloneable {
-    public static final String DEFAULT_GOAL_FILENAME_REFERENCE = "robot";
-    public static final String DEFAULT_GOAL_FILENAME = "robot.goal";
-    private static final long serialVersionUID = -4261058226493472776L;
+	public static final String DEFAULT_GOAL_FILENAME_REFERENCE = "robot";
+	public static final String DEFAULT_GOAL_FILENAME = "robot.goal";
+	private static final long serialVersionUID = -4261058226493472776L;
 
-    private String name = "Bot";
-    private EntityType controller = EntityType.AGENT;
-    private int amount = 1;
-    private int botSize = 2;
-    private int botSpeed = 100;
-    private int botBatteryCapacity = 10;
-    private int numberOfGrippers = 1;
-    private boolean batteryEnabled = false;
-    private boolean hasColorBlindHandicap = false;
-    private boolean hasGripperHandicap = false;
-    private boolean hasMoveSpeedHandicap = false;
-    private boolean hasSizeOverloadHandicap = false;
-    private String fileName = "*.goal";
-    private String referenceName = "";
+	// note, these values may differ from the default values in server.
+	private String name = "Bot";
+	private EntityType controller = EntityType.AGENT;
+	private int amount = 1;
+	private int botSize = 2;
+	private int botSpeed = 50;
+	private int botBatteryCapacity = 10;
+	private int numberOfGrippers = 1;
+	private boolean batteryEnabled = false;
+	private boolean hasColorBlindHandicap = false;
+	private boolean hasGripperHandicap = false;
+	private boolean hasMoveSpeedHandicap = false;
+	private boolean hasSizeOverloadHandicap = false;
+	private String fileName = "*.goal";
+	private String referenceName = "";
 
-    /**
-     * @return the default configuration of a Human
-     */
-    public static BotConfig createDefaultHumans() {
-        BotConfig bot = new BotConfig();
-        bot.setBotName("Human");
-        bot.setBotController(EntityType.HUMAN);
-        return bot;
-    }
+	/**
+	 * @return the default configuration of a Human
+	 */
+	public static BotConfig createDefaultHumans() {
+		BotConfig bot = new BotConfig();
+		bot.setBotName("Human");
+		bot.setBotController(EntityType.HUMAN);
+		return bot;
+	}
 
-    /**
-     * @return the default configuration of a robot
-     */
-    public static BotConfig createDefaultRobot() {
-        BotConfig bot = new BotConfig();
-        bot.setBotController(EntityType.AGENT);
-        return bot;
-    }
+	/**
+	 * @return the default configuration of a robot
+	 */
+	public static BotConfig createDefaultRobot() {
+		BotConfig bot = new BotConfig();
+		bot.setBotController(EntityType.AGENT);
+		return bot;
+	}
 
-    /**
-     * Calculate the discharge rate given the size and speed of the bot.
-     *
-     * @param size  the size of the bot
-     * @param speed the speed of the bot
-     * @return the discharge rate per step
-     */
-    public static double calculateDischargeRate(int size, int speed) {
-        return 0.0002 * size + 0.0004 * speed;
-    }
+	/**
+	 * Calculate the discharge rate given the size and speed of the bot.
+	 *
+	 * @param size
+	 *            the size of the bot
+	 * @param speed
+	 *            the speed of the bot
+	 * @return the discharge rate per step
+	 */
+	public static double calculateDischargeRate(int size, int speed) {
+		return 0.0002 * size + 0.0004 * speed;
+	}
 
-    public String getBotName() {
-        return this.name;
-    }
+	public String getBotName() {
+		return this.name;
+	}
 
-    @XmlElement
-    public void setBotName(String name) {
-        this.name = name;
-    }
+	@XmlElement
+	public void setBotName(String name) {
+		this.name = name;
+	}
 
-    public EntityType getBotController() {
-        return this.controller;
-    }
+	public EntityType getBotController() {
+		return this.controller;
+	}
 
-    @XmlElement
-    public void setBotController(EntityType controller) {
-        this.controller = controller;
-    }
+	@XmlElement
+	public void setBotController(EntityType controller) {
+		this.controller = controller;
+	}
 
-    public int getBotAmount() {
-        return this.amount;
-    }
+	public int getBotAmount() {
+		return this.amount;
+	}
 
-    @XmlElement
-    public void setBotAmount(int amount) {
-        this.amount = amount;
-    }
+	@XmlElement
+	public void setBotAmount(int amount) {
+		this.amount = amount;
+	}
 
-    public int getBotSize() {
-        return botSize;
-    }
+	public int getBotSize() {
+		return botSize;
+	}
 
-    @XmlElement
-    public void setBotSize(int newSize) {
-        botSize = newSize;
-    }
+	@XmlElement
+	public void setBotSize(int newSize) {
+		botSize = newSize;
+	}
 
-    public int getBotSpeed() {
-        return botSpeed;
-    }
+	public int getBotSpeed() {
+		return botSpeed;
+	}
 
-    @XmlElement
-    public void setBotSpeed(int newSpeed) {
-        botSpeed = newSpeed;
-    }
+	@XmlElement
+	public void setBotSpeed(int newSpeed) {
+		botSpeed = newSpeed;
+	}
 
-    public boolean isBatteryEnabled() {
-        return this.batteryEnabled;
-    }
+	public boolean isBatteryEnabled() {
+		return this.batteryEnabled;
+	}
 
-    @XmlElement
-    public void setBatteryEnabled(boolean batteryEnabled) {
-        this.batteryEnabled = batteryEnabled;
-    }
+	@XmlElement
+	public void setBatteryEnabled(boolean batteryEnabled) {
+		this.batteryEnabled = batteryEnabled;
+	}
 
-    public int getBotBatteryCapacity() {
-        return botBatteryCapacity;
-    }
+	public int getBotBatteryCapacity() {
+		return botBatteryCapacity;
+	}
 
-    @XmlElement
-    public void setBotBatteryCapacity(int newBatteryCapacity) {
-        botBatteryCapacity = newBatteryCapacity;
-    }
+	@XmlElement
+	public void setBotBatteryCapacity(int newBatteryCapacity) {
+		botBatteryCapacity = newBatteryCapacity;
+	}
 
-    public double getBotBatteryDischargeRate() {
-        return calculateDischargeRate(botSize, botSpeed);
-    }
+	public double getBotBatteryDischargeRate() {
+		return calculateDischargeRate(botSize, botSpeed);
+	}
 
-    public boolean getColorBlindHandicap() {
-        return hasColorBlindHandicap;
-    }
+	public boolean getColorBlindHandicap() {
+		return hasColorBlindHandicap;
+	}
 
-    @XmlElement
-    public void setColorBlindHandicap(boolean bool) {
-        hasColorBlindHandicap = bool;
-    }
+	@XmlElement
+	public void setColorBlindHandicap(boolean bool) {
+		hasColorBlindHandicap = bool;
+	}
 
-    public boolean getGripperHandicap() {
-        return hasGripperHandicap;
-    }
+	public boolean getGripperHandicap() {
+		return hasGripperHandicap;
+	}
 
-    @XmlElement
-    public void setGripperHandicap(boolean bool) {
-        hasGripperHandicap = bool;
-    }
+	@XmlElement
+	public void setGripperHandicap(boolean bool) {
+		hasGripperHandicap = bool;
+	}
 
-    public boolean getMoveSpeedHandicap() {
-        return hasMoveSpeedHandicap;
-    }
+	public boolean getMoveSpeedHandicap() {
+		return hasMoveSpeedHandicap;
+	}
 
-    @XmlElement
-    public void setMoveSpeedHandicap(boolean bool) {
-        hasMoveSpeedHandicap = bool;
-    }
+	@XmlElement
+	public void setMoveSpeedHandicap(boolean bool) {
+		hasMoveSpeedHandicap = bool;
+	}
 
-    public boolean getSizeOverloadHandicap() {
-        return hasSizeOverloadHandicap;
-    }
+	public boolean getSizeOverloadHandicap() {
+		return hasSizeOverloadHandicap;
+	}
 
-    @XmlElement
-    public void setSizeOverloadHandicap(boolean bool) {
-        hasSizeOverloadHandicap = bool;
-    }
+	@XmlElement
+	public void setSizeOverloadHandicap(boolean bool) {
+		hasSizeOverloadHandicap = bool;
+	}
 
-    public int getGrippers() {
-        return this.numberOfGrippers;
-    }
+	public int getGrippers() {
+		return this.numberOfGrippers;
+	}
 
-    @XmlElement
-    public void setGrippers(int grippers) {
-        this.numberOfGrippers = grippers;
-    }
+	@XmlElement
+	public void setGrippers(int grippers) {
+		this.numberOfGrippers = grippers;
+	}
 
-    /**
-     * Returns all the properties as a String.
-     *
-     * @return All the BotConfig properties.
-     */
-    public String bcToString() {
-        return name + controller + amount + botSize
-                + botSpeed + botBatteryCapacity
-                + numberOfGrippers + batteryEnabled + hasColorBlindHandicap
-                + hasGripperHandicap + hasMoveSpeedHandicap
-                + hasSizeOverloadHandicap + fileName + referenceName;
-    }
+	/**
+	 * Returns all the properties as a String.
+	 *
+	 * @return All the BotConfig properties.
+	 */
+	public String bcToString() {
+		return name + controller + amount + botSize + botSpeed
+				+ botBatteryCapacity + numberOfGrippers + batteryEnabled
+				+ hasColorBlindHandicap + hasGripperHandicap
+				+ hasMoveSpeedHandicap + hasSizeOverloadHandicap + fileName
+				+ referenceName;
+	}
 
-    public String getReferenceName() {
-        return referenceName;
-    }
+	public String getReferenceName() {
+		return referenceName;
+	}
 
-    @XmlElement
-    public void setReferenceName(String _referenceName) {
-        this.referenceName = _referenceName;
-    }
+	@XmlElement
+	public void setReferenceName(String _referenceName) {
+		this.referenceName = _referenceName;
+	}
 
-    public String getFileName() {
-        return fileName;
-    }
+	public String getFileName() {
+		return fileName;
+	}
 
-    @XmlElement
-    public void setFileName(String _fileName) {
-        this.fileName = _fileName;
-    }
+	@XmlElement
+	public void setFileName(String _fileName) {
+		this.fileName = _fileName;
+	}
 
-    @Override
-    public BotConfig clone() {
-        try {
-            return (BotConfig) super.clone();
-        } catch (CloneNotSupportedException e) {
-            return null;
-        }
-    }
+	@Override
+	public BotConfig clone() {
+		try {
+			return (BotConfig) super.clone();
+		} catch (CloneNotSupportedException e) {
+			return null;
+		}
+	}
 
 }
