ec01c1a78710ae7b06f7ce0396e6252e50770ea5
wouter <W.Pasman@TUDelft.nl>
2014-10-20 17:22:21 +0200

    #3205 config files are now read from the directory where the env.jar is.

--- bw4t/bw4t-client/src/main/java/nl/tudelft/bw4t/client/startup/ConfigFile.java
+++ bw4t/bw4t-client/src/main/java/nl/tudelft/bw4t/client/startup/ConfigFile.java
@@ -2,78 +2,98 @@ package nl.tudelft.bw4t.client.startup;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.xml.bind.JAXBException;
-
-import org.apache.log4j.Logger;
 
 import nl.tudelft.bw4t.scenariogui.BW4TClientConfig;
 import nl.tudelft.bw4t.util.XMLManager;
 
+import org.apache.log4j.Logger;
+
 /**
- * Reads the config file that can be passed to the client to
- * overwrite the InitParam variables.
+ * Reads the config file that can be passed to the client to overwrite the
+ * InitParam variables.
  */
 public class ConfigFile {
-    private static final Logger LOGGER = Logger.getLogger(ConfigFile.class);
-    
-    protected File file;
-    protected BW4TClientConfig config;
-    
-    /**
-     * Reads the given file as configuration file.
-     * @param filename the path and filename of the config to be used 
-     * @throws JAXBException If an error occurs while reading the xml config
-     * @throws FileNotFoundException if the param is null or the file can not be read
-     */
-    public ConfigFile(String filename) throws JAXBException, FileNotFoundException {
-    	file = new File(filename);
-    	
-    	if(filename == null || !file.canRead())
-    		throw new FileNotFoundException();
-        // Reads the configuration file and constructs a BW4TClientConfig from it:
-        config = (BW4TClientConfig) XMLManager.fromXML(filename, BW4TClientConfig.class);
-    }
-    
-    /**
-     * Gets the value for an init param based on the info read
-     * from this configuration file.
-     * @param param The init param.
-     * @return the value of the given param in the file or null if not set
-     */
-    public String getValue(InitParam param) {
-        switch (param) {
-        case CLIENTIP:
-            return config.getClientIp();
-        case CLIENTPORT:
-            return Integer.toString(config.getClientPort());
-        case GOAL:
-            return Boolean.toString(config.isUseGoal());
-        case LAUNCHGUI:
-            return Boolean.toString(config.isLaunchGui());
-        case SERVERIP:
-            return config.getServerIp();
-        case SERVERPORT:
-            return Integer.toString(config.getServerPort());
-        case MAP:
-            return config.getMapFile();
-        default:
-            LOGGER.info(param.name() + " is not supported by the config file");
-            return null;
-        }
-    }
-    
-    /**
-     * Gets the map file that was read from the init configuration file.
-     * 
-     * @return The location to the map file or {@code null} if no init file was read.
-     */
-    public String getMapFile() {
-        return config.getMapFile();
-    }
+	private static final Logger LOGGER = Logger.getLogger(ConfigFile.class);
 
-    public BW4TClientConfig getConfig() {
-        return config;
-    }
+	protected File file;
+	protected BW4TClientConfig config;
+
+	/**
+	 * Reads the given file as configuration file.
+	 * 
+	 * @param filename
+	 *            the path and filename of the config to be used #3205 This will
+	 *            be relative to the directory where our environment jar file is
+	 *            located.
+	 * @throws JAXBException
+	 *             If an error occurs while reading the xml config
+	 * @throws FileNotFoundException
+	 *             if the param is null or the file can not be read
+	 */
+	public ConfigFile(String filename) throws JAXBException,
+			FileNotFoundException {
+
+		URL url = getClass().getProtectionDomain().getCodeSource()
+				.getLocation();
+		Path p = Paths.get(url.getFile());
+		file = p.getParent().resolve(filename).toFile();
+
+		if (file == null || !file.canRead())
+			throw new FileNotFoundException("could not read config file "
+					+ filename + " inside " + url);
+		// Reads the configuration file and constructs a BW4TClientConfig from
+		// it:
+		config = (BW4TClientConfig) XMLManager.fromXML(file,
+				BW4TClientConfig.class);
+	}
+
+	/**
+	 * Gets the value for an init param based on the info read from this
+	 * configuration file.
+	 * 
+	 * @param param
+	 *            The init param.
+	 * @return the value of the given param in the file or null if not set
+	 */
+	public String getValue(InitParam param) {
+		switch (param) {
+		case CLIENTIP:
+			return config.getClientIp();
+		case CLIENTPORT:
+			return Integer.toString(config.getClientPort());
+		case GOAL:
+			return Boolean.toString(config.isUseGoal());
+		case LAUNCHGUI:
+			return Boolean.toString(config.isLaunchGui());
+		case SERVERIP:
+			return config.getServerIp();
+		case SERVERPORT:
+			return Integer.toString(config.getServerPort());
+		case MAP:
+			return config.getMapFile();
+		default:
+			LOGGER.info(param.name() + " is not supported by the config file");
+			return null;
+		}
+	}
+
+	/**
+	 * Gets the map file that was read from the init configuration file.
+	 * 
+	 * @return The location to the map file or {@code null} if no init file was
+	 *         read.
+	 */
+	public String getMapFile() {
+		return config.getMapFile();
+	}
+
+	public BW4TClientConfig getConfig() {
+		return config;
+	}
 
 }
