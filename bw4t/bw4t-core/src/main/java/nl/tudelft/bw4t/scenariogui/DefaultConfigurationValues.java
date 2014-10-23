08f0bb4e7c4bb1609de0adf2cc3aeb5008dba25a
wouter <W.Pasman@TUDelft.nl>
2014-10-23 13:50:26 +0200

    #3296 enable collisions now has default off also in ClientInfo object and in GUI (from which the ClientInfo pulls its default).

--- bw4t/bw4t-core/src/main/java/nl/tudelft/bw4t/scenariogui/DefaultConfigurationValues.java
+++ bw4t/bw4t-core/src/main/java/nl/tudelft/bw4t/scenariogui/DefaultConfigurationValues.java
@@ -1,56 +1,58 @@
 package nl.tudelft.bw4t.scenariogui;
 
 /**
- * The default values for in the GUI.
+ * The default values. Both for the core (which calls
+ * nl.tudelft.bw4t.server.ClientInfo which uses this default) and for in the GUI
  */
 public enum DefaultConfigurationValues {
-    DEFAULT_SERVER_IP("localhost"),
-    DEFAULT_CLIENT_IP("localhost"),
-    DEFAULT_SERVER_PORT("8000"),
-    DEFAULT_CLIENT_PORT("2000"),
-    USE_GUI("FALSE"),
-    VISUALIZE_PATHS("FALSE"),
-    ENABLE_COLLISIONS("FALSE"),
-    USE_GOAL("TRUE"),
-    MAP_FILE(""),
-    COLLISIONS_ENABLED("TRUE");
+	DEFAULT_SERVER_IP("localhost"), // (// are here to guide the auto
+	DEFAULT_CLIENT_IP("localhost"), // formatter)
+	DEFAULT_SERVER_PORT("8000"), //
+	DEFAULT_CLIENT_PORT("2000"), //
+	USE_GUI("FALSE"), //
+	VISUALIZE_PATHS("FALSE"), //
+	ENABLE_COLLISIONS("FALSE"), //
+	USE_GOAL("TRUE"), //
+	MAP_FILE(""), //
+	COLLISIONS_ENABLED("FALSE");
 
-    private String value;
+	private String value;
 
-    /**
-     * Constructs a new default configuration.
-     *
-     * @param newValue The new default value.
-     */
-    DefaultConfigurationValues(final String newValue) {
-        this.value = newValue;
-    }
+	/**
+	 * Constructs a new default configuration.
+	 *
+	 * @param newValue
+	 *            The new default value.
+	 */
+	DefaultConfigurationValues(final String newValue) {
+		this.value = newValue;
+	}
 
-    /**
-     * Gets the value as a string.
-     *
-     * @return The value.
-     */
-    public String getValue() {
-        return value;
-    }
+	/**
+	 * Gets the value as a string.
+	 *
+	 * @return The value.
+	 */
+	public String getValue() {
+		return value;
+	}
 
-    /**
-     * Gets the value as an int.
-     *
-     * @return The value as an int.
-     */
-    public int getIntValue() {
-        return Integer.parseInt(value);
-    }
+	/**
+	 * Gets the value as an int.
+	 *
+	 * @return The value as an int.
+	 */
+	public int getIntValue() {
+		return Integer.parseInt(value);
+	}
 
-    /**
-     * Gets the value as a boolean.
-     *
-     * @return The value as a boolean.
-     */
-    public boolean getBooleanValue() {
-        return Boolean.parseBoolean(value);
-    }
+	/**
+	 * Gets the value as a boolean.
+	 *
+	 * @return The value as a boolean.
+	 */
+	public boolean getBooleanValue() {
+		return Boolean.parseBoolean(value);
+	}
 
 }
