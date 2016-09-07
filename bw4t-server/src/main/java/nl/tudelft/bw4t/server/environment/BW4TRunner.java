aab0128879a21669162d52205512e94cc69bc652
Wouter1 <W.Pasman@TUDelft.nl>
2016-09-07 10:18:19 +0200

    #4026 "scenario" server optino is now called "repast"

--- bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/BW4TRunner.java
+++ bw4t-server/src/main/java/nl/tudelft/bw4t/server/environment/BW4TRunner.java
@@ -19,127 +19,137 @@ import repast.simphony.parameter.SweeperProducer;
 import repast.simphony.scenario.ScenarioLoadException;
 
 /**
- * This class implements the repast {@link Runner}. This handles the calls to the repast stepping - when do bots move,
- * how often, and pausing etc. This is modified copy of TestRunner_2 (see #2009 and #2236). I did not give this much
- * thought, I just plugged it in and it did what I hoped for - being able to control Repast. Otherwise,
- * scheduling/running is not done here at all, but from the {@link BW4TEnvironment} directly by calling {@link #step()}.
+ * This class implements the repast {@link Runner}. This handles the calls to
+ * the repast stepping - when do bots move, how often, and pausing etc. This is
+ * modified copy of TestRunner_2 (see #2009 and #2236). I did not give this much
+ * thought, I just plugged it in and it did what I hoped for - being able to
+ * control Repast. Otherwise, scheduling/running is not done here at all, but
+ * from the {@link BW4TEnvironment} directly by calling {@link #step()}.
  */
 public class BW4TRunner extends AbstractRunner {
 
-    /**
-     * The log4j logger, logs to the console.
-     */
-    private static final Logger LOGGER = Logger.getLogger(BW4TRunner.class);
+	/**
+	 * The log4j logger, logs to the console.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(BW4TRunner.class);
 
-    private RunEnvironmentBuilder runEnvironmentBuilder;
-    protected Controller bw4tController;
-    protected boolean pauseRunner = false;
-    protected Object monitor = new Object();
-    protected SweeperProducer producer;
-    private ISchedule schedule;
-    
-    public BW4TRunner() {
-        runEnvironmentBuilder = new DefaultRunEnvironmentBuilder(this, true);
-        bw4tController = new DefaultController(runEnvironmentBuilder);
-        bw4tController.setScheduleRunner(this);
-    }
+	private RunEnvironmentBuilder runEnvironmentBuilder;
+	protected Controller bw4tController;
+	protected boolean pauseRunner = false;
+	protected Object monitor = new Object();
+	protected SweeperProducer producer;
+	private ISchedule schedule;
 
-    /**
-     *  Loads a scenario
-     * @param scenarioDir 
-     * @throws ScenarioLoadException 
-     */
-    public void load(File scenarioDir) throws ScenarioLoadException {
-        if (scenarioDir.exists()) {
-            BatchScenarioLoader loader = new BatchScenarioLoader(scenarioDir);
-            ControllerRegistry registry = loader.load(runEnvironmentBuilder);
-            bw4tController.setControllerRegistry(registry);
-        } else {
-            LOGGER.fatal("Scenario directory was not found.");
-            return;
-        }
+	public BW4TRunner() {
+		runEnvironmentBuilder = new DefaultRunEnvironmentBuilder(this, true);
+		bw4tController = new DefaultController(runEnvironmentBuilder);
+		bw4tController.setScheduleRunner(this);
+	}
 
-        bw4tController.batchInitialize();
-        bw4tController.runParameterSetters(null);
-    }
+	/**
+	 * Loads a scenario
+	 * 
+	 * @param scenarioDir
+	 * @throws ScenarioLoadException
+	 */
+	public void load(File scenarioDir) throws ScenarioLoadException {
+		if (scenarioDir.exists()) {
+			BatchScenarioLoader loader = new BatchScenarioLoader(scenarioDir);
+			ControllerRegistry registry = loader.load(runEnvironmentBuilder);
+			bw4tController.setControllerRegistry(registry);
+		} else {
+			LOGGER.fatal("Scenario directory " + scenarioDir + " was not found.");
+			return;
+		}
 
-    /**
-     * intitialize it
-     */
-    public void runInitialize() {
-        BW4TParameters params = new BW4TParameters();
-        bw4tController.runInitialize(params);
-        schedule = RunState.getInstance().getScheduleRegistry().getModelSchedule();
-        fireStartedMessage();
-    }
+		bw4tController.batchInitialize();
+		bw4tController.runParameterSetters(null);
+	}
 
-    /**
-     * clean up the run
-     */
-    public void cleanUpRun() {
-        bw4tController.runCleanup();
-    }
+	/**
+	 * intitialize it
+	 */
+	public void runInitialize() {
+		BW4TParameters params = new BW4TParameters();
+		bw4tController.runInitialize(params);
+		schedule = RunState.getInstance().getScheduleRegistry().getModelSchedule();
+		fireStartedMessage();
+	}
 
-    /**
-     * clean up the batch
-     */
-    public void cleanUpBatch() {
-        bw4tController.batchCleanup();
-    }
+	/**
+	 * clean up the run
+	 */
+	public void cleanUpRun() {
+		bw4tController.runCleanup();
+	}
 
-    // returns the tick count of the next scheduled item
-    public double getNextScheduledTime() {
-        return ((Schedule) RunEnvironment.getInstance().getCurrentSchedule()).peekNextAction().getNextTime();
-    }
+	/**
+	 * clean up the batch
+	 */
+	public void cleanUpBatch() {
+		bw4tController.batchCleanup();
+	}
 
-    /**
-     * see {@link ISchedule#getModelActionCount()}.
-     * @return Modelcount
-     */
-    public int getModelActionCount() {
-        return schedule.getModelActionCount();
-    }
+	// returns the tick count of the next scheduled item
+	public double getNextScheduledTime() {
+		return ((Schedule) RunEnvironment.getInstance().getCurrentSchedule()).peekNextAction().getNextTime();
+	}
 
-    /**
-     * see {@link ISchedule#getActionCount()}.
-     * @return actionCount
-     */
-    public int getActionCount() {
-        return schedule.getActionCount();
-    }
+	/**
+	 * see {@link ISchedule#getModelActionCount()}.
+	 * 
+	 * @return Modelcount
+	 */
+	public int getModelActionCount() {
+		return schedule.getModelActionCount();
+	}
 
-    /**
-     *  Step the schedule
-     */
-    public void step() {
-        schedule.execute();
-    }
+	/**
+	 * see {@link ISchedule#getActionCount()}.
+	 * 
+	 * @return actionCount
+	 */
+	public int getActionCount() {
+		return schedule.getActionCount();
+	}
 
-    /**
-     *  stop the schedule
-     */
-    public void stop() {
-        if (schedule != null) {
-            schedule.executeEndActions();
-            fireStoppedMessage();
-        }
-    }
+	/**
+	 * Step the schedule
+	 */
+	@Override
+	public void step() {
+		schedule.execute();
+	}
 
-    /**
-     * set ending
-     * @param fin 
-     */
-    public void setFinishing(boolean fin) {
-        schedule.setFinishing(fin);
-    }
-    
-    /**
-     * Not sure if this is needed. 
-     * @param toExecuteOn 
-     */
-    public void execute(RunState toExecuteOn) {
-        // required AbstractRunner stub. We will control the
-        // schedule directly.
-    }
+	/**
+	 * stop the schedule
+	 */
+	@Override
+	public void stop() {
+		if (schedule != null) {
+			schedule.executeEndActions();
+			fireStoppedMessage();
+		}
+	}
+
+	/**
+	 * set ending
+	 * 
+	 * @param fin
+	 */
+	public void setFinishing(boolean fin) {
+		schedule.setFinishing(fin);
+	}
+
+	/**
+	 * Not sure if this is needed.
+	 * 
+	 * @param toExecuteOn
+	 */
+	@Override
+	public void execute(RunState toExecuteOn) {
+		// required AbstractRunner stub. We will control the
+		// schedule directly.
+	}
 
 }
