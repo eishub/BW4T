a93236b7cd6b3e77ab029de1b4a6fc11725859c7
Vincent Koeman <v.j.koeman@tudelft.nl>
2018-08-23 12:16:23 +0200

    fix unit tests after previous commit

--- bw4t-server/src/test/java/nl/tudelft/bw4t/server/eis/RobotEntityTest.java
+++ bw4t-server/src/test/java/nl/tudelft/bw4t/server/eis/RobotEntityTest.java
@@ -25,7 +25,6 @@ import repast.simphony.context.DefaultContext;
 import repast.simphony.space.continuous.NdPoint;
 
 public class RobotEntityTest {
-
 	private IRobot mockRobot = Mockito.mock(IRobot.class);
 	private RobotEntity robot;
 	private BW4TEnvironment env = Mockito.mock(BW4TEnvironment.class);
@@ -39,70 +38,69 @@ public class RobotEntityTest {
 	@Before
 	public void createRobotEntity() throws NoSuchMethodException, SecurityException, IllegalAccessException,
 			IllegalArgumentException, InvocationTargetException, NoSuchFieldException {
-		robot = new RobotEntity(mockRobot, true);
-		when(mockRobot.getLocation()).thenReturn(new NdPoint(1, 1));
-		when(mockRobot.getHolding()).thenReturn(new Stack<Block>());
+		this.robot = new RobotEntity(this.mockRobot, true);
+		when(this.mockRobot.getLocation()).thenReturn(new NdPoint(1, 1));
+		when(this.mockRobot.getHolding()).thenReturn(new Stack<Block>());
 
-		when(mockRobot.isConnected()).then(new Answer<Boolean>() {
+		when(this.mockRobot.isConnected()).then(new Answer<Boolean>() {
 			@Override
 			public Boolean answer(InvocationOnMock invocation) throws Throwable {
 				// HACK but I don't know how to check the actual counts.
 				// so we just check that connected was called and return true.
-				Mockito.verify(mockRobot).connect();
+				Mockito.verify(RobotEntityTest.this.mockRobot).connect();
 				return true;
 			}
 		});
 
-		method = robot.getClass().getDeclaredMethod("getVisible", Class.class);
-		method.setAccessible(true);
+		this.method = this.robot.getClass().getDeclaredMethod("getVisible", Class.class);
+		this.method.setAccessible(true);
 
-		method3 = robot.getClass().getDeclaredMethod("getSizes");
-		method3.setAccessible(true);
+		this.method3 = this.robot.getClass().getDeclaredMethod("getSizes");
+		this.method3.setAccessible(true);
 
-		method2 = BW4TEnvironment.class.getDeclaredMethod("setInstance", BW4TEnvironment.class);
-		method2.setAccessible(true);
-		method2.invoke(null, env);
-		when(env.getContext()).thenReturn(context);
+		this.method2 = BW4TEnvironment.class.getDeclaredMethod("setInstance", BW4TEnvironment.class);
+		this.method2.setAccessible(true);
+		this.method2.invoke(null, this.env);
+		when(this.env.getContext()).thenReturn(this.context);
+		when(this.env.hasContext()).thenReturn(true);
 
-		field = RobotEntity.class.getDeclaredField("ourRobotRoom");
-		field.setAccessible(true);
-
+		this.field = RobotEntity.class.getDeclaredField("ourRobotRoom");
+		this.field.setAccessible(true);
 	}
 
 	@Test
 	public void getRobotTest() {
-		assertEquals(mockRobot, robot.getRobotObject());
+		assertEquals(this.mockRobot, this.robot.getRobotObject());
 	}
 
 	@Test
 	public void connectTest() {
-		robot.connect();
-		Mockito.verify(mockRobot).connect();
-		robot.reset();
-		Mockito.verify(mockRobot).getHolding();
-		Mockito.verify(mockRobot).moveTo(1, 1);
+		this.robot.connect();
+		Mockito.verify(this.mockRobot).connect();
+		this.robot.reset();
+		Mockito.verify(this.mockRobot).getHolding();
+		Mockito.verify(this.mockRobot).moveTo(1, 1);
 	}
 
 	@Test
 	public void disconnectTest() {
-		robot.connect();
-		robot.disconnect();
-		Mockito.verify(mockRobot).disconnect();
+		this.robot.connect();
+		this.robot.disconnect();
+		Mockito.verify(this.mockRobot).disconnect();
 	}
 
 	@Test
 	public void getVisibleBlocksTest()
 			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		assertEquals(new HashSet<Block>(), method.invoke(robot, Block.class));
-		robot.initializePerceptionCycle();
-		field.set(robot, mockRoom);
-		assertNotNull(mockRoom);
-		when(robot.getRoom()).thenReturn("in");
-		assertNotNull(robot.getRoom());
+		assertEquals(new HashSet<Block>(), this.method.invoke(this.robot, Block.class));
+		this.robot.initializePerceptionCycle();
+		this.field.set(this.robot, this.mockRoom);
+		assertNotNull(this.mockRoom);
+		when(this.robot.getRoom()).thenReturn("in");
+		assertNotNull(this.robot.getRoom());
 
 	}
 
 	// getSize is not testable
 	// getAt is not testable
-
 }
--- bw4t-server/src/test/java/nl/tudelft/bw4t/server/util/RoomLocatorTest.java
+++ bw4t-server/src/test/java/nl/tudelft/bw4t/server/util/RoomLocatorTest.java
@@ -27,86 +27,85 @@ import repast.simphony.context.DefaultContext;
 /** Tests the functions in the utility class RoomLocator. */
 @RunWith(Parameterized.class)
 public class RoomLocatorTest {
+	@Mock
+	private static BlocksRoom BLOCKSROOM = mock(BlocksRoom.class);
+	@Mock
+	private static DropZone DROPZONE = mock(DropZone.class);
+	@Mock
+	private static BW4TEnvironment env = mock(BW4TEnvironment.class);
 
-    @Mock private static BlocksRoom BLOCKSROOM = mock(BlocksRoom.class);
-    @Mock private static DropZone DROPZONE = mock(DropZone.class);
-    @Mock private static BW4TEnvironment env = mock(BW4TEnvironment.class);
-    
-    /** A minor offset is done on certain elements in order to fulfill requirements of the definition of insideness. */
-    private static double OFFSET = 1E-5;
-    private static Context<Object> context = new DefaultContext<>();
-    
-    private double x;
-    private double y;
-    private Room expectedOutAt;
-    private Room expectedOutFor;
-    
-    @BeforeClass
-    public static void setUp() throws NoSuchMethodException, SecurityException,
-            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        Method method = BW4TEnvironment.class.getDeclaredMethod("setInstance", BW4TEnvironment.class);
-        method.setAccessible(true);
-        method.invoke(null, env);
-        when(env.getContext()).thenReturn(context);
+	/**
+	 * A minor offset is done on certain elements in order to fulfill requirements
+	 * of the definition of insideness.
+	 */
+	private static double OFFSET = 1E-5;
+	private static Context<Object> context = new DefaultContext<>();
 
-        context.add(BLOCKSROOM);
-        context.add(DROPZONE);
+	private double x;
+	private double y;
+	private Room expectedOutAt;
+	private Room expectedOutFor;
 
-        when(BLOCKSROOM.getBoundingBox()).thenReturn(new Rectangle(0, 0, 1, 1));
-        when(DROPZONE.getBoundingBox()).thenReturn(new Rectangle(10, 10, 1, 1));
-    }
-    
-    /**
-     * Set the values to use for the current test.
-     * @param x - The x coordinate to search with.
-     * @param y - The y coordinate to search with.
-     * @param expectedOutAt - The expected output of the getRoomAt function.
-     * @param expectedOutFor - The expected output of the getRoomFor function.
-     */
-    public RoomLocatorTest(double x, double y, Room expectedOutAt, Room expectedOutFor){
-        this.x = x;
-        this.y = y;
-        this.expectedOutAt = expectedOutAt;
-        this.expectedOutFor = expectedOutFor;
-    }
+	@BeforeClass
+	public static void setUp() throws NoSuchMethodException, SecurityException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+		Method method = BW4TEnvironment.class.getDeclaredMethod("setInstance", BW4TEnvironment.class);
+		method.setAccessible(true);
+		method.invoke(null, env);
+		when(env.getContext()).thenReturn(context);
+		when(env.hasContext()).thenReturn(true);
 
-    /** We test the getRoomAt method for correctness. */
-    @Test
-    public void getRoomAtTest() {
-        Room result = RoomLocator.getRoomAt(x, y);
-        assertTrue((result == null && expectedOutAt == null)
-                || (result != null && result.equals(expectedOutAt)));
-    }
+		context.add(BLOCKSROOM);
+		context.add(DROPZONE);
 
-    /** We test the getRoomFor method for correctness. */
-    @Test
-    public void getRoomForTest() {
-        Room result = RoomLocator.getRoomFor(x, y);
-        assertTrue((result == null && expectedOutFor == null)
-                || (result != null && result.equals(expectedOutFor)));
-    }
-    
-    /** @return The list of parameters to use for testing */
-    @Parameters(name = "{index} - {0},{1}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {0,             0,              BLOCKSROOM, BLOCKSROOM},
-                {0,             1 - OFFSET,     BLOCKSROOM, BLOCKSROOM},
-                {1 - OFFSET,    0,              BLOCKSROOM, BLOCKSROOM},
-                {1 - OFFSET,    1 - OFFSET,     BLOCKSROOM, BLOCKSROOM},
-                {0.5,           0.5,            BLOCKSROOM, BLOCKSROOM},
-                {10,            10,             DROPZONE,   null},
-                {10,            11 - OFFSET,    DROPZONE,   null},
-                {11 - OFFSET,   10,             DROPZONE,   null},
-                {11 - OFFSET,   11 - OFFSET,    DROPZONE,   null},
-                {10.5,          10.5,           DROPZONE,   null},
-                {0,             0 - OFFSET,     null,       null},
-                {0 - OFFSET,    0,              null,       null},
-                {0 - OFFSET,    0 - OFFSET,     null,       null},
-                {0,             1,              null,       null},
-                {1,             0,              null,       null},
-                {1,             1,              null,       null}
-        });
-    }
+		when(BLOCKSROOM.getBoundingBox()).thenReturn(new Rectangle(0, 0, 1, 1));
+		when(DROPZONE.getBoundingBox()).thenReturn(new Rectangle(10, 10, 1, 1));
+	}
 
+	/**
+	 * Set the values to use for the current test.
+	 *
+	 * @param x
+	 *            - The x coordinate to search with.
+	 * @param y
+	 *            - The y coordinate to search with.
+	 * @param expectedOutAt
+	 *            - The expected output of the getRoomAt function.
+	 * @param expectedOutFor
+	 *            - The expected output of the getRoomFor function.
+	 */
+	public RoomLocatorTest(double x, double y, Room expectedOutAt, Room expectedOutFor) {
+		this.x = x;
+		this.y = y;
+		this.expectedOutAt = expectedOutAt;
+		this.expectedOutFor = expectedOutFor;
+	}
+
+	/** We test the getRoomAt method for correctness. */
+	@Test
+	public void getRoomAtTest() {
+		Room result = RoomLocator.getRoomAt(this.x, this.y);
+		assertTrue((result == null && this.expectedOutAt == null)
+				|| (result != null && result.equals(this.expectedOutAt)));
+	}
+
+	/** We test the getRoomFor method for correctness. */
+	@Test
+	public void getRoomForTest() {
+		Room result = RoomLocator.getRoomFor(this.x, this.y);
+		assertTrue((result == null && this.expectedOutFor == null)
+				|| (result != null && result.equals(this.expectedOutFor)));
+	}
+
+	/** @return The list of parameters to use for testing */
+	@Parameters(name = "{index} - {0},{1}")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] { { 0, 0, BLOCKSROOM, BLOCKSROOM },
+				{ 0, 1 - OFFSET, BLOCKSROOM, BLOCKSROOM }, { 1 - OFFSET, 0, BLOCKSROOM, BLOCKSROOM },
+				{ 1 - OFFSET, 1 - OFFSET, BLOCKSROOM, BLOCKSROOM }, { 0.5, 0.5, BLOCKSROOM, BLOCKSROOM },
+				{ 10, 10, DROPZONE, null }, { 10, 11 - OFFSET, DROPZONE, null }, { 11 - OFFSET, 10, DROPZONE, null },
+				{ 11 - OFFSET, 11 - OFFSET, DROPZONE, null }, { 10.5, 10.5, DROPZONE, null },
+				{ 0, 0 - OFFSET, null, null }, { 0 - OFFSET, 0, null, null }, { 0 - OFFSET, 0 - OFFSET, null, null },
+				{ 0, 1, null, null }, { 1, 0, null, null }, { 1, 1, null, null } });
+	}
 }
